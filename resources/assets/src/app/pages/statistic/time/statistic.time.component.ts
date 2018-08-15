aa1df46362c531649d2bce0a2296b901cef5ec8b
Alexander Yanchuk <alexanderyanchuk95@gmail.com>
2018-08-15 15:54:21 +0700

    Fix csv export in the manager dashboard

--- resources/assets/src/app/pages/statistic/time/statistic.time.component.ts
+++ resources/assets/src/app/pages/statistic/time/statistic.time.component.ts
@@ -578,8 +578,26 @@ export class StatisticTimeComponent implements OnInit {
             return cells.join(',');
         });
 
-        const content = 'data:text/csv;charset=utf-8,' + header.join(',') + '\n' + lines.join('\n');
-        window.open(encodeURI(content));
+        const filename = 'data.csv';
+        const content = header.join(',') + '\n' + lines.join('\n');
+        const blob = new Blob([content], { type: 'text/csv;charset=utf-8;' });
+        if (navigator.msSaveBlob) { // IE 10+
+            navigator.msSaveBlob(blob, filename);
+        } else {
+            const link = document.createElement('a');
+            if (link.download !== undefined) { // feature detection
+                // Browsers that support HTML5 download attribute
+                const url = URL.createObjectURL(blob);
+                link.setAttribute('href', url);
+                link.setAttribute('download', filename);
+                link.style.visibility = 'hidden';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            } else {
+                window.open(encodeURI('data:text/csv;charset=utf-8,' + content));
+            }
+        }
     }
 
     setLoading(loading: boolean = true) {
