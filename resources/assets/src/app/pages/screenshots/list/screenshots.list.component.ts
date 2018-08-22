2ef33cbe8d78df1c2a76f65136311759897494d3
Alexander Yanchuk <alexanderyanchuk95@gmail.com>
2018-08-22 16:17:46 +0700

    Update screenshots view

--- resources/assets/src/app/pages/screenshots/list/screenshots.list.component.ts
+++ resources/assets/src/app/pages/screenshots/list/screenshots.list.component.ts
@@ -1,15 +1,20 @@
 import {Component, DoCheck, IterableDiffers, OnInit, ViewChild, OnDestroy} from '@angular/core';
 import {ApiService} from '../../../api/api.service';
 import {ScreenshotsService} from '../screenshots.service';
-import { Screenshot } from '../../../models/screenshot.model';
+import { BsModalService } from 'ngx-bootstrap';
 import { AllowedActionsService } from '../../roles/allowed-actions.service';
+
+import { Screenshot } from '../../../models/screenshot.model';
+
+import { ItemsListComponent } from '../../items.list.component';
+import { splitMatchedQueriesDsl } from '@angular/core/src/view/util';
 
 @Component({
     selector: 'app-screenshots-list',
     templateUrl: './screenshots.list.component.html',
     styleUrls: ['./screenshots.list.component.css']
 })
-export class ScreenshotsListComponent implements OnInit, DoCheck, OnDestroy {
+export class ScreenshotsListComponent extends ItemsListComponent implements OnInit, DoCheck, OnDestroy {
     @ViewChild('loading') element: any;
 
     userId: number = null;
@@ -19,16 +24,32 @@ export class ScreenshotsListComponent implements OnInit, DoCheck, OnDestroy {
 
     chunksize = 32;
     offset = 0;
-    screenshots: Screenshot[] = [];
     screenshotLoading = false;
     scrollHandler: any = null;
     countFail = 0;
 
+    protected _itemsChunked = [];
+    get itemsChunked() {
+        if (this._itemsChunked.length > 0) {
+            return this._itemsChunked;
+        }
+
+        const result = [];
+        const chunkSize = 6;
+        for (let i = 0, len = this.itemsArray.length; i < len; i += chunkSize) {
+            result.push(this.itemsArray.slice(i, i + chunkSize));
+        }
+
+        return this._itemsChunked = result;
+    }
+
     constructor(protected api: ApiService,
                 protected itemService: ScreenshotsService,
+                protected modalService: BsModalService,
                 protected allowedAction: AllowedActionsService,
                 differs: IterableDiffers
     ) {
+        super(api, itemService, modalService, allowedAction);
         this.differUser = differs.find([]).create(null);
         this.differProject = differs.find([]).create(null);
     }
@@ -42,71 +63,17 @@ export class ScreenshotsListComponent implements OnInit, DoCheck, OnDestroy {
     ngDoCheck() {
         const changeUserId = this.differUser.diff([this.userId]);
         const changeProjectId = this.differProject.diff([this.projectId]);
-        const filter = {'with': 'timeInterval,timeInterval.task,timeInterval.user'};
 
         if (changeUserId || changeProjectId) {
-            if (this.userId) {
-                filter['user_id'] = ['=', this.userId];
-            }
-
-            if (this.projectId) {
-                filter['project_id'] = ['=', this.projectId];
-            }
-
-            //this.itemService.getItems(this.setData.bind(this), filter ? filter : null);
+            this.offset = 0;
+            this.setItems([]);
+            this.countFail = 0;
+            this.loadNext();
         }
     }
 
     ngOnDestroy() {
         window.removeEventListener('scroll', this.scrollHandler, false);
-    }
-
-    can(action: string ): boolean {
-        return this.allowedAction.can(action);
-    }
-
-    getScreenshots(limit, offset, callback, userId) {
-        const itemsArray: Screenshot[] = [];
-
-        return this.api.send(
-            'screenshots/list',
-            {
-                'user_id': userId,
-                'limit': limit,
-                'offset': offset,
-                'with': 'timeInterval,timeInterval.task,timeInterval.task.project'
-            },
-            (result) => {
-                result.forEach((itemFromApi) => {
-                    itemsArray.push(new Screenshot(itemFromApi));
-                });
-
-                callback(itemsArray);
-            });
-    }
-
-    setData(result) {
-        if (result.length > 0) {
-            this.offset += this.chunksize;
-
-            for (const screenshot of result) {
-                this.screenshots.push(new Screenshot(screenshot));
-            }
-        } else {
-            this.countFail += 1;
-        }
-
-        this.screenshotLoading = false;
-    }
-
-    loadNext() {
-        if (this.screenshotLoading || this.countFail > 3) {
-          return;
-        }
-
-        const user: any = this.api.getUser() ? this.api.getUser() : null;
-        this.screenshotLoading = true;
-        this.getScreenshots(this.chunksize, this.offset, this.setData.bind(this), user.id);
     }
 
     onScrollDown() {
@@ -122,10 +89,51 @@ export class ScreenshotsListComponent implements OnInit, DoCheck, OnDestroy {
         this.loadNext();
     }
 
+    setItems(items) {
+        super.setItems(items);
+        this._itemsChunked = [];
+    }
+
     minutes(datetime) {
         const regex = /\d{4}-\d{2}-\d{2} \d{2}:(\d{2}:\d{2})/;
         const matches = datetime.match(regex);
 
         return matches[1];
+    }
+
+    loadNext() {
+        if (this.screenshotLoading || this.countFail > 3) {
+          return;
+        }
+
+        const params = {
+            'with': 'timeInterval,timeInterval.task,timeInterval.user',
+            'limit': this.chunksize,
+            'offset': this.offset,
+        };
+
+        if (this.userId) {
+            params['user_id'] = ['=', this.userId];
+        }
+
+        if (this.projectId) {
+            params['project_id'] = ['=', this.projectId];
+        }
+
+        this.screenshotLoading = true;
+        try {
+            this.itemService.getItems((items) => {
+                if (items.length > 0) {
+                    this.setItems(this.itemsArray.concat(items));
+                } else {
+                    this.countFail += 1;
+                }
+
+                this.screenshotLoading = false;
+            }, params);
+        } catch {
+            this.countFail += 1;
+            this.screenshotLoading = false;
+        }
     }
 }
