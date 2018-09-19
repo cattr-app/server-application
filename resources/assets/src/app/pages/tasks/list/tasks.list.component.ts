068fe5fc7b8dbde9643e4564886557bd835bea20
Alexander Yanchuk <alexanderyanchuk95@gmail.com>
2018-09-19 14:56:26 +0600

    Add ordering on the tasks list page

--- resources/assets/src/app/pages/tasks/list/tasks.list.component.ts
+++ resources/assets/src/app/pages/tasks/list/tasks.list.component.ts
@@ -1,4 +1,4 @@
-import {Component, DoCheck, IterableDiffers, OnInit} from '@angular/core';
+import {Component, DoCheck, IterableDiffers, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 import {ApiService} from '../../../api/api.service';
 import {TasksService} from '../tasks.service';
 import {Task} from '../../../models/task.model';
@@ -8,10 +8,19 @@ import {AllowedActionsService} from '../../roles/allowed-actions.service';
 import {ProjectsService} from '../../projects/projects.service';
 import {LocalStorage} from '../../../api/storage.model';
 
+enum TasksOrder {
+    IdAsc,
+    IdDesc,
+    ProjectAsc,
+    ProjectDesc,
+    NameAsc,
+    NameDesc,
+}
+
 @Component({
     selector: 'app-tasks-list',
     templateUrl: './tasks.list.component.html',
-    styleUrls: ['../../items.component.scss']
+    styleUrls: ['./tasks.list.component.scss', '../../items.component.scss']
 })
 export class TasksListComponent extends ItemsListComponent implements OnInit, DoCheck {
 
@@ -22,6 +31,7 @@ export class TasksListComponent extends ItemsListComponent implements OnInit, Do
     differUser: any;
     differProject: any;
     directProject: any = [];
+    order: TasksOrder = TasksOrder.IdAsc;
 
     constructor(api: ApiService,
                 taskService: TasksService,
@@ -52,24 +62,55 @@ export class TasksListComponent extends ItemsListComponent implements OnInit, Do
         this.directProject = items;
     }
 
+    load() {
+        const filter = {
+            'with': 'project',
+        };
+
+        if (this.userId && this.userId.length) {
+            filter['user_id'] = ['=', this.userId];
+        }
+
+        if (this.projectId && this.projectId.length) {
+            filter['project_id'] = ['=', this.projectId];
+        }
+
+        this.itemService.getItems(this.setItems.bind(this), filter ? filter : { 'active': 1 });
+        this.projectService.getItems(this.setProjects.bind(this), this.can('/projects/full_access')
+            ? []
+            : { 'direct_relation': 1 });
+    }
+
+    setItems(result: Task[]) {
+        this.itemsArray = result.sort((a, b) => {
+            switch(this.order) {
+                default:
+                case TasksOrder.IdAsc: return a.id - b.id;
+                case TasksOrder.IdDesc: return b.id - a.id;
+                case TasksOrder.ProjectAsc: return a.project.name.localeCompare(b.project.name);
+                case TasksOrder.ProjectDesc: return b.project.name.localeCompare(a.project.name);
+                case TasksOrder.NameAsc: return a.task_name.localeCompare(b.task_name);
+                case TasksOrder.NameDesc: return b.task_name.localeCompare(a.task_name);
+            }
+        });
+    }
+
+    setOrder(order: string) {
+        switch (order) {
+            default:
+            case 'id': this.order = this.order === TasksOrder.IdAsc ? TasksOrder.IdDesc : TasksOrder.IdAsc; break;
+            case 'project': this.order = this.order === TasksOrder.ProjectAsc ? TasksOrder.ProjectDesc : TasksOrder.ProjectAsc; break;
+            case 'name': this.order = this.order === TasksOrder.NameAsc ? TasksOrder.NameDesc : TasksOrder.NameAsc; break;
+        }
+        this.load();
+    }
+
     ngDoCheck() {
         const changeUserId = this.differUser.diff([this.userId]);
         const changeProjectId = this.differProject.diff([this.projectId]);
-        const filter = { 'with': 'project' };
 
         if (changeUserId || changeProjectId) {
-            if (this.userId && this.userId.length) {
-                filter['user_id'] = ['=', this.userId];
-            }
-
-            if (this.projectId && this.projectId.length) {
-                filter['project_id'] = ['=', this.projectId];
-            }
-
-            this.itemService.getItems(this.setItems.bind(this), filter ? filter : { 'active': 1 });
-            this.projectService.getItems(this.setProjects.bind(this), this.can('/projects/full_access')
-                ? []
-                : { 'direct_relation': 1 });
+            this.load();
         }
     }
 }
