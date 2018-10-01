10fb51a6a13e51a8e1bbd0cf6a92bd968f3640f8
Alexander Yanchuk <alexanderyanchuk95@gmail.com>
2018-10-01 13:52:46 +0700

    Add access check for screenshot thumbnails

--- app/Http/Controllers/ScreenshotController.php
+++ app/Http/Controllers/ScreenshotController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use App\Models\Screenshot;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Auth;
-use App\User;
 
 /**
  * Class ScreenshotController
@@ -22,47 +22,44 @@ class ScreenshotController extends Controller
         $path = $request->path();
         $screenshot = Screenshot::with('timeInterval')
             ->where('path', $path)
-            ->firstOrFail();
-
-        $screenshot_full_path = storage_path('app/' . $screenshot->path);
-        $screenshot_user_id = $screenshot->timeInterval->user_id;
+            ->first();
+        if (!isset($screenshot)) {
+            return response(null, 404);
+        }
 
         $user = $request->user();
-
-        // Allow root to see all screenshots.
-        if (Role::can($user, 'screenshots', 'full_access')) {
-            return response()->file($screenshot_full_path);
+        if (!$screenshot->access($user)) {
+            return response(null, 403);
         }
 
-        // Allow user to see own screenshots.
-        if (Role::can($user, 'screenshots', 'list') && $user->id === $screenshot_user_id) {
-            return response()->file($screenshot_full_path);
+        $full_path = storage_path('app/' . $screenshot->path);
+        if (!file_exists($full_path)) {
+            return response(null, 404);
         }
 
-        // Allow manager to see screenshots of related users.
-        if (Role::can($user, 'screenshots', 'manager_access')) {
-            if (Role::can($user, 'projects', 'relations')) {
-                $attached_project_ids = $user->projects->pluck('id');
-                $related_user_ids = User::whereHas('timeIntervals', function ($query) use ($attached_project_ids) {
-                    $query->whereHas('task', function ($query) use ($attached_project_ids) {
-                        $query->whereHas('project', function ($query) use ($attached_project_ids) {
-                            $query->whereIn('id', $attached_project_ids);
-                        });
-                    });
-                })->select('id')->get('id')->pluck('id');
-                if ($related_user_ids->contains($screenshot_user_id)) {
-                    return response()->file($screenshot_full_path);
-                }
-            }
+        return response()->file($full_path);
+    }
 
-            if (Role::can($user, 'projects', 'relations')) {
-                $attached_user_ids = $user->attached_users->pluck('id');
-                if ($attached_user_ids->contains($screenshot_user_id)) {
-                    return response()->file($screenshot_full_path);
-                }
-            }
+    public function thumbnail(Request $request)
+    {
+        $path = $request->path();
+        $screenshot = Screenshot::with('timeInterval')
+            ->where('thumbnail_path', $path)
+            ->first();
+        if (!isset($screenshot)) {
+            return response(null, 404);
         }
 
-        return response(null, 403);
+        $user = $request->user();
+        if (!$screenshot->access($user)) {
+            return response(null, 403);
+        }
+
+        $full_path = storage_path('app/' . $screenshot->thumbnail_path);
+        if (!file_exists($full_path)) {
+            return response(null, 404);
+        }
+
+        return response()->file($full_path);
     }
 }
