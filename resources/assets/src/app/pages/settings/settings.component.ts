566456d74b3680ece4a800d4e9377d51e995f730
Alexander Yanchuk <alexanderyanchuk95@gmail.com>
2018-10-09 17:43:22 +0700

    Move integrations to the settings page

--- resources/assets/src/app/pages/settings/settings.component.ts
+++ resources/assets/src/app/pages/settings/settings.component.ts
@@ -1,49 +1,65 @@
-import {ApiService} from '../../api/api.service';
+import { ApiService } from '../../api/api.service';
 import { Component, OnInit } from '@angular/core';
-import {Message} from 'primeng/components/common/api';
-import {TranslateService} from '@ngx-translate/core';
-import {LocalStorage} from '../../api/storage.model';
+import { Message } from 'primeng/components/common/api';
+import { TranslateService } from '@ngx-translate/core';
+import { LocalStorage } from '../../api/storage.model';
 
+interface RedmineStatus {
+    id: number;
+    name: string;
+    is_active: boolean;
+}
 
 @Component({
     selector: 'app-settings',
     templateUrl: './settings.component.html',
     styleUrls: ['../../app.component.scss'],
 })
-
-
-export class SettingsComponent implements OnInit {
+export class SettingsComponent {
     msgs: Message[] = [];
 
     languages = [
-        {'code': 'en', 'title': 'English'},
-        {'code': 'ru', 'title': 'Русский'},
-        {'code': 'dk', 'title': 'Dansk'},
-    ]
-
+        { 'code': 'en', 'title': 'English' },
+        { 'code': 'ru', 'title': 'Русский' },
+        { 'code': 'dk', 'title': 'Dansk' },
+    ];
     language: string = 'en';
 
-    constructor(private api: ApiService,
-        protected translate: TranslateService
-        ) {
-        let lang = translate.currentLang;
+    redmineUrl: string;
+    redmineApiKey: string;
+    redmineStatuses: RedmineStatus[] = [];
 
-        if (lang == null) {
-            lang = translate.defaultLang;
-        }
-        this.language = lang;
-    }
+    constructor(
+        private api: ApiService,
+        protected translate: TranslateService,
+    ) {
+        this.language = translate.currentLang
+            ? translate.currentLang
+            : translate.defaultLang;
 
-    ngOnInit() {
+        this.api.getSettings([], result => {
+            this.redmineUrl = result.redmine_url;
+            this.redmineApiKey = result.redmine_api_key;
+            this.redmineStatuses = result.redmine_statuses;
+        });
     }
 
     onSubmit() {
-
         if (typeof this.language != null) {
             this.translate.use(this.language);
             LocalStorage.getStorage().set('language', this.language);
         }
+
+        this.api.sendSettings({
+            'redmine_url': this.redmineUrl,
+            'redmine_key': this.redmineApiKey,
+            'redmine_statuses': this.redmineStatuses,
+        }, () => {
+            this.msgs = [{
+                severity: 'success',
+                summary: 'Success Message',
+                detail: 'Settings have been updated',
+            }];
+        });
     }
-
-
 }
