2c60f35f1986a1a2a99b011bcfd6d6675e43d2e1
Vladislav Dyshakov <kawaii.nekotech@gmail.com>
2018-10-15 13:05:36 +0700

    Add TimeInterval json structure check

--- tests/Feature/v1/TimeIntervalControllerTest.php
+++ tests/Feature/v1/TimeIntervalControllerTest.php
@@ -19,9 +19,19 @@ class TimeIntervalControllerTest extends TestCase
             "end_at"    => "2017-05-11T00:00:00+08:00"
         ];
 
+        $expectedFields = [
+            "interval" => [
+                "id", "task_id", "start_at", "end_at",
+                "created_at", "updated_at", "count_mouse", "count_keyboard",
+                "user_id"
+            ]
+
+        ];
+
         $response = $this->postJson("/api/v1/time-intervals/create", $data, $headers);
 
         $response->assertStatus(200);
+        $response->assertJsonStructure($expectedFields);
     }
 
     public function test_Destroy_ExpectPass()
@@ -30,14 +40,27 @@ class TimeIntervalControllerTest extends TestCase
             "Authorization" => "Bearer " . $this->getAdminToken()
         ];
 
-        $createResponse = $this->postJson("/api/v1/time-intervals/destroy");
-
-        $data = [
-            "id" => 1
+        $createData = [
+            "task_id"   => 1,
+            "user_id"   => 1,
+            "start_at"  => "2017-05-11T00:00:00+08:00",
+            "end_at"    => "2017-05-11T00:00:00+08:00"
         ];
 
-        $response = $this->postJson("/api/v1/time-intervals/destroy", [], $headers);
+        $createResponse = $this->postJson("/api/v1/time-intervals/create", $createData, $headers);
+
+        $data = [
+            "id" => $createResponse->json("interval.id")
+        ];
+
+        $expectedFields = [
+            "message"
+        ];
+
+        $response = $this->postJson("/api/v1/time-intervals/remove", $data, $headers);
+
         $response->assertStatus(200);
+        $response->assertJsonStructure($expectedFields);
     }
 
     public function test_Edit_ExpectPass()
@@ -54,10 +77,18 @@ class TimeIntervalControllerTest extends TestCase
             "end_at"    => "2018-10-03T12:00:00+02:00"
         ];
 
+        $expectedFields = [
+            "res" => [
+                "id", "task_id", "start_at", "end_at", "created_at",
+                "updated_at", "deleted_at", "count_mouse", "count_keyboard",
+                "user_id"
+            ]
+        ];
+
         $response = $this->postJson("/api/v1/time-intervals/edit", $data, $headers);
-        echo "Wrong: " . var_export($response->content(), true);
 
         $response->assertStatus(200);
+        $response->assertJsonStructure($expectedFields);
     }
 
     public function test_List_ExpectPass()
@@ -66,8 +97,17 @@ class TimeIntervalControllerTest extends TestCase
             "Authorization" => "Bearer " . $this->getAdminToken()
         ];
 
+        $expectedFields = [
+            "*" => [
+                "id", "task_id", "start_at", "end_at", "created_at", "updated_at",
+                "deleted_at", "count_mouse", "count_keyboard", "user_id"
+            ]
+        ];
+
         $response = $this->postJson("/api/v1/time-intervals/list", [], $headers);
+
         $response->assertStatus(200);
+        $response->assertJsonStructure($expectedFields);
     }
 
     public function test_Show_ExpectPass()
@@ -80,8 +120,14 @@ class TimeIntervalControllerTest extends TestCase
             "id" => 1
         ];
 
+        $expectedFields = [
+            "id", "task_id", "start_at", "end_at", "created_at", "updated_at",
+            "deleted_at", "count_mouse", "count_keyboard", "user_id"
+        ];
+
         $response = $this->postJson("/api/v1/time-intervals/show", $data, $headers);
 
         $response->assertStatus(200);
+        $response->assertJsonStructure($expectedFields);
     }
 }
