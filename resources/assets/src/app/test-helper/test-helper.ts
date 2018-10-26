1d97865509654bff61565d1f7524f656437b6b92
Vycheslav Sokolov <vycheslavsokolov2013@gmail.com>
2018-10-26 13:27:24 +0600

    Added loader manager storage, called clear() before load admin/user storage.

--- resources/assets/src/app/test-helper/test-helper.ts
+++ resources/assets/src/app/test-helper/test-helper.ts
@@ -3,6 +3,7 @@ import {LocalStorage} from '../api/storage.model';
 const localStorage = LocalStorage.getStorage();
 
 export function loadAdminStorage() {
+  localStorage.clear();
   localStorage.set('attached_users', [
     {
       'id': 2,
@@ -482,6 +483,7 @@ export function loadAdminStorage() {
 }
 
 export function loadUserStorage() {
+  localStorage.clear();
   localStorage.set('allowed_actions', [
     {
       'object': 'project-report',
@@ -694,3 +696,445 @@ export function loadUserStorage() {
   localStorage.set('token', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTUzOTAwNjAwNCwiZXhwIjoxNTM5MDA5NjA0LCJuYmYiOjE1MzkwMDYwMDQsImp0aSI6IjZ0TFpXRE02ZlFwYmlCdk8iLCJzdWIiOjEsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.kuhSY8Ae7k5vSHHXelN0gxEcJpYO6268Sv-XA1H47ZI');
   localStorage.set('tokenType', 'bearer');
 }
+
+export function loadManagerStorage() {
+    localStorage.clear();
+    localStorage.set('attached_users', [
+        {
+          "id":13,
+          "full_name":"User 1",
+          "first_name":"User 1",
+          "last_name":null,
+          "email":"user1@example.com",
+          "url":null,
+          "company_id":1,
+          "level":null,
+          "payroll_access":1,
+          "billing_access":1,
+          "avatar":null,
+          "screenshots_active":1,
+          "manual_time":0,
+          "permanent_tasks":0,
+          "computer_time_popup":300,
+          "poor_time_popup":null,
+          "blur_screenshots":0,
+          "web_and_app_monitoring":1,
+          "webcam_shots":0,
+          "screenshots_interval":9,
+          "user_role_value":null,
+          "active":1,
+          "deleted_at":null,
+          "created_at":"2018-08-07 05:45:11",
+          "updated_at":"2018-10-18 00:26:17",
+          "role_id":2,
+          "timezone":"Asia/Novosibirsk",
+          "pivot":{
+            "project_id":6,
+            "user_id":13
+          }
+        },
+        
+        {"id":22,
+        "full_name":"manager2",
+        "first_name":"manager",
+        "last_name":"2",
+        "email":"manager2@example.com",
+        "url":null,
+        "company_id":null,
+        "level":null,
+        "payroll_access":null,
+        "billing_access":null,
+        "avatar":null,
+        "screenshots_active":1,
+        "manual_time":0,
+        "permanent_tasks":null,
+        "computer_time_popup":5,
+        "poor_time_popup":null,
+        "blur_screenshots":null,
+        "web_and_app_monitoring":null,
+        "webcam_shots":null,
+        "screenshots_interval":5,
+        "user_role_value":null,
+        "active":1,
+        "deleted_at":null,
+        "created_at":"2018-10-24 10:13:12",
+        "updated_at":"2018-10-24 10:13:12",
+        "role_id":5,
+        "timezone":"Asia/Omsk",
+        "pivot":{
+          "project_id":6,
+          "user_id":22
+        }
+      },
+      
+      {
+        "id":1,
+        "full_name":"Ad Min",
+        "first_name":"Ad",
+        "last_name":"Min",
+        "email":"admin@example.com",
+        "url":null,
+        "company_id":1,
+        "level":"admin",
+        "payroll_access":1,
+        "billing_access":1,
+        "avatar":null,
+        "screenshots_active":1,
+        "manual_time":0,
+        "permanent_tasks":0,
+        "computer_time_popup":300,
+        "poor_time_popup":null,
+        "blur_screenshots":0,
+        "web_and_app_monitoring":1,
+        "webcam_shots":0,
+        "screenshots_interval":9,
+        "user_role_value":null,
+        "active":1,
+        "deleted_at":null,
+        "created_at":"2018-08-07 04:51:08",
+        "updated_at":"2018-08-31 10:38:15",
+        "role_id":1,"timezone":"Asia/Omsk"
+      },
+      
+      {
+        "id":13,
+        "full_name":"User 1",
+        "first_name":"User 1",
+        "last_name":null,
+        "email":"user1@example.com",
+        "url":null,
+        "company_id":1,
+        "level":null,
+        "payroll_access":1,
+        "billing_access":1,
+        "avatar":null,
+        "screenshots_active":1,
+        "manual_time":0,
+        "permanent_tasks":0,
+        "computer_time_popup":300,
+        "poor_time_popup":null,
+        "blur_screenshots":0,
+        "web_and_app_monitoring":1,
+        "webcam_shots":0,
+        "screenshots_interval":9,
+        "user_role_value":null,
+        "active":1,
+        "deleted_at":null,
+        "created_at":"2018-08-07 05:45:11",
+        "updated_at":"2018-10-18 00:26:17",
+        "role_id":2,
+        "timezone":"Asia/Novosibirsk"
+      },
+      
+      {
+        "id":14,
+        "full_name":"User 2",
+        "first_name":"User 2",
+        "last_name":null,
+        "email":"user2@example.com",
+        "url":null,"company_id":1,
+        "level":null,"payroll_access":1,
+        "billing_access":1,
+        "avatar":null,
+        "screenshots_active":1,
+        "manual_time":0,
+        "permanent_tasks":0,
+        "computer_time_popup":300,
+        "poor_time_popup":null,
+        "blur_screenshots":0,
+        "web_and_app_monitoring":1,
+        "webcam_shots":0,
+        "screenshots_interval":9,
+        "user_role_value":null,
+        "active":1,
+        "deleted_at":null,
+        "created_at":"2018-08-07 05:45:13",
+        "updated_at":"2018-08-31 06:38:32",
+        "role_id":2,
+        "timezone":"Asia/Novosibirsk"
+      },
+      
+      {
+        "id":19,
+        "full_name":"Alexander Yanchuk",
+        "first_name":"Alexander",
+        "last_name":"Yanchuk",
+        "email":"alexanderyanchuk95@gmail.com",
+        "url":null,
+        "company_id":null,
+        "level":"2",
+        "payroll_access":1,
+        "billing_access":null,
+        "avatar":null,
+        "screenshots_active":1,
+        "manual_time":0,
+        "permanent_tasks":0,
+        "computer_time_popup":300,
+        "poor_time_popup":null,
+        "blur_screenshots":0,
+        "web_and_app_monitoring":1,
+        "webcam_shots":0,
+        "screenshots_interval":5,
+        "user_role_value":null,
+        "active":1,
+        "deleted_at":null,
+        "created_at":"2018-08-28 05:01:55",
+        "updated_at":"2018-10-22 02:17:44",
+        "role_id":5,
+        "timezone":"Asia/Omsk"
+      }
+    ]);
+    localStorage.set('attached_projects', [
+      {
+        "id":1,
+        "company_id":0,
+        "name":"Qui nihil.",
+        "description":"Laboriosam excepturi harum et sapiente. Veritatis incidunt neque aut dolor nesciunt. Expedita est totam voluptate cumque. Quia occaecati non blanditiis nihil non. Maxime nihil atque deleniti nemo. Necessitatibus vel minus accusantium aperiam. Aliquid qui neque vel facilis sint. Sunt laudantium assumenda quaerat accusantium laborum ipsa. Repellat voluptatem repellat sed voluptatem. Aspernatur voluptate nulla soluta qui officia sit sint maxime. Voluptatem aut vel ut est voluptas omnis. Et natus quae et fuga quo. Sapiente non non voluptatem alias in aliquid. Officia non animi est tempore reprehenderit ea. Amet rerum facere illum et quae. Quia iste et ad voluptas.",
+        "deleted_at":null,
+        "created_at":"2018-08-07 04:51:08",
+        "updated_at":"2018-08-07 04:51:08"
+      },
+    
+      {
+        "id":6,
+        "company_id":4,
+        "name":"Amazing Time",
+        "description":"",
+        "deleted_at":null,
+        "created_at":"2018-08-17 07:08:40",
+        "updated_at":"2018-08-17 07:08:40"
+      }
+    ]);
+    localStorage.set('allowed_actions', [
+      {
+        "object":"attached-users",
+        "action":"list",
+        "name":"Attached User relation list"
+      },
+      
+      {
+        "object":"dashboard",
+        "action":"manager_access",
+        "name":"Dashboard manager access"
+      },
+      
+      {
+        "object":"project-report",
+        "action":"list",
+        "name":"Projects report list"
+      },
+      
+      {
+        "object":"project-report",
+        "action":"manager_access",
+        "name":"Projects report manager access"
+      },
+      
+      {
+        "object":"project-report",
+        "action":"projects",
+        "name":"Projects report related projects"
+      },
+      
+      {
+        "object":"projects",
+        "action":"list",
+        "name":"Project list"
+      },
+      
+      {
+        "object":"projects",
+        "action":"relations",
+        "name":"Project list attached to user"
+      },
+      
+      {
+        "object":"projects",
+        "action":"show",
+        "name":"Project show"
+      },
+      
+      {
+        "object":"projects-roles",
+        "action":"list",
+        "name":"Project Role relation list"
+      },
+      
+      {
+        "object":"projects-users",
+        "action":"list",
+        "name":"Project User relation list"
+      },
+      
+      {
+        "object":"roles",
+        "action":"allowed-rules",
+        "name":"Role allowed rule list"
+      },
+      
+      {
+        "object":"roles",
+        "action":"list",
+        "name":"Role list"
+      },
+      
+      {
+        "object":"screenshots",
+        "action":"dashboard",
+        "name":"Screenshot list at dashboard"
+      },
+      
+      {
+        "object":"screenshots",
+        "action":"edit",
+        "name":"Screenshot edit"
+      },
+      
+      {
+        "object":"screenshots",
+        "action":"list",
+        "name":"Screenshot list"
+      },
+      
+      {
+        "object":"screenshots",
+        "action":"manager_access",
+        "name":"Screenshots manager access"
+      },
+      
+      {
+        "object":"screenshots",
+        "action":"remove",
+        "name":"Screenshot remove"
+      },
+      
+      {
+        "object":"screenshots",
+        "action":"show",
+        "name":"Screenshot show"
+      },
+      
+      {
+        "object":"tasks",
+        "action":"dashboard",
+        "name":"Task list at dashboard"
+      },
+      
+      {
+        "object":"tasks",
+        "action":"list",
+        "name":"Task list"
+      },
+      
+      {
+        "object":"tasks",
+        "action":"show",
+        "name":"Task show"
+      },
+      
+      {
+        "object":"time",
+        "action":"project",
+        "name":"Time by project"
+      },
+      
+      {
+        "object":"time",
+        "action":"task",
+        "name":"Time by single task"
+      },
+      
+      {
+        "object":"time",
+        "action":"task-user",
+        "name":"Time by single task and user"
+      },
+      
+      {
+        "object":"time",
+        "action":"tasks",
+        "name":"Time by tasks"
+      },
+      
+      {
+        "object":"time-duration",
+        "action":"list",
+        "name":"Time duration list"
+      },
+      
+      {
+        "object":"time-intervals",
+        "action":"list",
+        "name":"Time interval list"
+      },
+      
+      {
+        "object":"time-intervals",
+        "action":"manager_access",
+        "name":"Time intervals manager access"
+      },
+      
+      {
+        "object":"time-intervals",
+        "action":"show",
+        "name":"Time interval show"
+      },
+      
+      {
+        "object":"time-use-report",
+        "action":"list",
+        "name":"Time use report list"
+      },
+     
+      {
+        "object":"users",
+        "action":"list",
+        "name":"User list"
+      },
+      
+      {
+        "object":"users",
+        "action":"relations",
+        "name":"Attached users list"
+      },
+      
+      {
+        "object":"users",
+        "action":"show",
+        "name":"User show"
+      }
+    ]);
+    localStorage.set('user', 
+    {
+      "id":12,
+      "full_name":"Manager",
+      "first_name":"Manager",
+      "last_name":null,
+      "email":"manager@example.com",
+      "url":null,
+      "company_id":1,
+      "level":null,
+      "payroll_access":1,
+      "billing_access":1,
+      "avatar":null,
+      "screenshots_active":1,
+      "manual_time":0,
+      "permanent_tasks":0,
+      "computer_time_popup":300,
+      "poor_time_popup":null,
+      "blur_screenshots":0,
+      "web_and_app_monitoring":1,
+      "webcam_shots":0,
+      "screenshots_interval":9,
+      "user_role_value":null,
+      "active":1,
+      "deleted_at":null,
+      "created_at":"2018-08-07 05:45:10",
+      "updated_at":"2018-10-18 00:25:49",
+      "role_id":5,
+      "timezone":"Asia/Novosibirsk"
+    } 
+    );
+    localStorage.set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hbWF6aW5nLXRpbWUubG9jOjgwMDBcL2FwaVwvYXV0aFwvbG9naW4iLCJpYXQiOjE1NDA1MzY4NjMsImV4cCI6MTU0MDU0MDQ2MywibmJmIjoxNTQwNTM2ODYzLCJqdGkiOiJWODdMZkRLeG1vTURDWk9YIiwic3ViIjoxMiwicHJ2IjoiODdlMGFmMWVmOWZkMTU4MTJmZGVjOTcxNTNhMTRlMGIwNDc1NDZhYSJ9.Z2uK4u2rUGADHyHr2Q6yvZ0KoPjToAgDYxYc9z884X0");
+    localStorage.set('tokenType', 'bearer');
+  }
