458ec093d96db0028b720023105a4ecffd09dead
Vycheslav Sokolov <vycheslavsokolov2013@gmail.com>
2018-10-31 13:48:51 +0600

    Added tests for managers (Dashboard component, Navigation component).

--- resources/assets/src/app/navigation/navigation.component.spec.ts
+++ resources/assets/src/app/navigation/navigation.component.spec.ts
@@ -10,7 +10,7 @@ import {NavigationComponent} from './navigation.component';
 import {AppRoutingModule} from '../app-routing.module';
 import {ProjectsService} from '../pages/projects/projects.service';
 import {AllowedActionsService} from '../pages/roles/allowed-actions.service';
-import {loadAdminStorage, loadUserStorage} from '../test-helper/test-helper';
+import {loadAdminStorage, loadUserStorage, loadManagerStorage} from '../test-helper/test-helper';
 
 
 describe('Navigation component(Admin)', () => {
@@ -40,7 +40,81 @@ describe('Navigation component(Admin)', () => {
       component.setAuth(true);
       fixture.detectChanges();
       nav = fixture.debugElement.query(By.all()).nativeElement;
-      console.log(nav);
+    });
+  }));
+
+  it('should be created', async(() => {
+    expect(component).toBeTruthy();
+  }));
+
+  it('has menu item "dashboard"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.dashboard');
+  }));
+
+  it('has menu item "projects report"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.projectsreport');
+  }));
+
+  it('has menu item "projects time-use-report"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.time-use-report');
+  }));
+
+  it('has menu item "projects"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.projects');
+  }));
+
+  it('has menu item "tasks"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.tasks');
+  }));
+
+  it('has menu item "users"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.users');
+  }));
+
+  it('has menu item "screenshots"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.screenshots');
+  }));
+
+  it('has menu item "role"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.role');
+  }));
+
+  it('has menu item "settings"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.settings');
+  }));
+
+  it('has menu item "logout"', async(() => {
+    expect(nav.innerHTML).toContain('navigation.logout');
+  }));
+});
+
+describe('Navigation component(Manager)', () => {
+  let component, fixture;
+  let nav;
+
+  beforeEach(async(() => {
+    loadManagerStorage();
+    TestBed.configureTestingModule({
+      declarations: [NavigationComponent, ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        ApiService,
+        HttpClient,
+        HttpHandler,
+        {provide: Router, useClass: AppRoutingModule},
+        Location,
+        {provide: LocationStrategy, useClass: PathLocationStrategy},
+        {provide: APP_BASE_HREF, useValue: '/'},
+        AllowedActionsService,
+        ProjectsService
+      ]
+    })
+      .compileComponents().then(() => {
+      fixture = TestBed.createComponent(NavigationComponent);
+      component = fixture.debugElement.componentInstance;
+      component.setAuth(true);
+      fixture.detectChanges();
+      nav = fixture.debugElement.query(By.all()).nativeElement;
     });
   }));
 
--- resources/assets/src/app/pages/dashboard/dashboard.component.spec.ts
+++ resources/assets/src/app/pages/dashboard/dashboard.component.spec.ts
@@ -10,15 +10,63 @@ import {Location, LocationStrategy, PathLocationStrategy, APP_BASE_HREF} from '@
 import {AllowedActionsService} from '../roles/allowed-actions.service';
 import {By} from '@angular/platform-browser';
 import {LocalStorage} from '../../api/storage.model';
-import {loadAdminStorage, loadUserStorage} from '../../test-helper/test-helper';
+import {loadAdminStorage, loadUserStorage, loadManagerStorage} from '../../test-helper/test-helper';
 import {TranslateFakeLoader, TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+
+describe('DashboardComponent(Admin)', () => {
+  let fixture, component;
+  
+  beforeEach(async(() => {
+    loadAdminStorage();
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: TranslateFakeLoader}
+        }), TabsModule.forRoot(),
+      ],
+      declarations: [DashboardComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        ApiService,
+        HttpClient,
+        HttpHandler,
+        {provide: Router, useClass: AppRoutingModule},
+        Location,
+        {provide: LocationStrategy, useClass: PathLocationStrategy},
+        {provide: APP_BASE_HREF, useValue: '/'},
+        AllowedActionsService,
+      ],
+    })
+      .compileComponents().then(() => {
+      fixture = TestBed.createComponent(DashboardComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should has "own" tab', () => {
+    component.ngOnInit();
+    component.ngAfterViewInit();
+    const el  = fixture.debugElement.query(By.all()).nativeElement;
+    expect(el.innerHTML).toContain('control.own');
+  });
+
+  it('should has "Team" tab', () => {
+    const el  = fixture.debugElement.query(By.all()).nativeElement;
+    expect(el.innerHTML).toContain('control.team');
+  });
+});
 
 describe('DashboardComponent(Manager)', () => {
   let fixture, component;
   
   beforeEach(async(() => {
-    loadAdminStorage();
+    loadManagerStorage();
     TestBed.configureTestingModule({
       imports: [
         TranslateModule.forRoot({
