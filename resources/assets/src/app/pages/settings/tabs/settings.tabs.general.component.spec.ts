79a385091618f92d9f24165ce27452bc3981d7fc
Vycheslav Sokolov <vycheslavsokolov2013@gmail.com>
2018-11-02 16:03:48 +0600

    Add spec general settings

---
+++ resources/assets/src/app/pages/settings/tabs/settings.tabs.general.component.spec.ts
@@ -0,0 +1,166 @@
+import {TestBed, async} from '@angular/core/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {TranslateFakeLoader, TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AppRoutingModule} from '../../../app-routing.module';
+import {APP_BASE_HREF, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {loadAdminStorage, loadUserStorage, loadManagerStorage} from '../../../test-helper/test-helper';
+import {Location} from '@angular/common';
+import {Observable} from '../../../../../../../node_modules/rxjs';
+import {GeneralComponent} from './settings.tabs.general.component'
+import {ApiService} from '../../../api/api.service';
+import {FormsModule} from '@angular/forms';
+import {NG_SELECT_DEFAULT_CONFIG, NgSelectModule} from '@ng-select/ng-select';
+import {AppComponent} from '../../../app.component';
+import {TabsModule} from 'ngx-bootstrap/tabs';
+import {By} from '@angular/platform-browser';
+
+describe('General component in Settings (User)', () => {
+  let component, fixture;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [GeneralComponent, AppComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        NgSelectModule, FormsModule,
+        NgxPaginationModule,
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: TranslateFakeLoader}
+        }), TabsModule.forRoot()
+      ],
+      providers: [
+        ApiService,
+        HttpClient,
+        HttpHandler,
+        {provide: Router, useClass: AppRoutingModule},
+        Location,
+        {provide: LocationStrategy, useClass: PathLocationStrategy},
+        {provide: APP_BASE_HREF, useValue: '/'},
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: Observable.of({id: 123})
+          }
+        },
+      ]
+    })
+      .compileComponents().then(() => {
+      loadUserStorage();
+      fixture = TestBed.createComponent(GeneralComponent);
+      component = fixture.debugElement.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('should be created', async(() => {
+    expect(component).toBeTruthy();
+  }));
+
+  it('general settings contains language settings', async(() => {
+    const el = fixture.debugElement.query(By.css("form")).nativeElement;
+      expect(el.innerHTML).toContain('settings.lang');
+      expect(el.innerHTML).toContain('settings.select-lang');
+      expect(el.innerHTML).toContain('control.save');
+  }));
+});
+
+describe('General component in Settings (Manager)', () => {
+  let component, fixture;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [GeneralComponent, AppComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        NgSelectModule, FormsModule,
+        NgxPaginationModule,
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: TranslateFakeLoader}
+        }), TabsModule.forRoot(),
+      ],
+      providers: [
+        ApiService,
+        HttpClient,
+        HttpHandler,
+        {provide: Router, useClass: AppRoutingModule},
+        Location,
+        {provide: LocationStrategy, useClass: PathLocationStrategy},
+        {provide: APP_BASE_HREF, useValue: '/'},
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: Observable.of({id: 123})
+          }
+        },
+      ]
+    })
+      .compileComponents().then(() => {
+      loadManagerStorage();
+      fixture = TestBed.createComponent(GeneralComponent);
+      component = fixture.debugElement.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('should be created', async(() => {
+    expect(component).toBeTruthy();
+  }));
+
+  it('general settings contains language settings', async(() => {
+    const el = fixture.debugElement.query(By.css("form")).nativeElement;
+      expect(el.innerHTML).toContain('settings.lang');
+      expect(el.innerHTML).toContain('settings.select-lang');
+      expect(el.innerHTML).toContain('control.save');
+  }));
+});
+
+describe('General component in Settings (Admin)', () => {
+  let component, fixture;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [GeneralComponent, AppComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        NgSelectModule, FormsModule,
+        NgxPaginationModule,
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: TranslateFakeLoader}
+        }), TabsModule.forRoot(),
+      ],
+      providers: [
+        ApiService,
+        HttpClient,
+        HttpHandler,
+        {provide: Router, useClass: AppRoutingModule},
+        Location,
+        {provide: LocationStrategy, useClass: PathLocationStrategy},
+        {provide: APP_BASE_HREF, useValue: '/'},
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: Observable.of({id: 123})
+          }
+        },
+      ]
+    })
+      .compileComponents().then(() => {
+      loadAdminStorage();
+      fixture = TestBed.createComponent(GeneralComponent);
+      component = fixture.debugElement.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('should be created', async(() => {
+    expect(component).toBeTruthy();
+  }));
+
+  it('general settings contains language settings', async(() => {
+    const el = fixture.debugElement.query(By.css("form")).nativeElement;
+      expect(el.innerHTML).toContain('settings.lang');
+      expect(el.innerHTML).toContain('settings.select-lang');
+      expect(el.innerHTML).toContain('control.save');
+  }));
+
+});
