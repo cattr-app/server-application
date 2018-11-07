62172de79710d8b7cd2f7c046683d5f1073ac561
Vycheslav Sokolov <vycheslavsokolov2013@gmail.com>
2018-11-07 12:46:17 +0600

    Added tests for calendar in Date-range-selector.

--- resources/assets/src/app/date-range-selector/date-range-selector.component.spec.ts
+++ resources/assets/src/app/date-range-selector/date-range-selector.component.spec.ts
@@ -5,12 +5,13 @@ import { HttpClient, HttpHandler } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { APP_BASE_HREF, Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { By } from '@angular/platform-browser';
-import { LocalStorage } from '../api/storage.model';
 import { DateRangeSelectorComponent } from './date-range-selector.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { ProjectsService } from '../pages/projects/projects.service';
 import { AllowedActionsService } from '../pages/roles/allowed-actions.service';
 import { TranslateService, TranslateModule, TranslateLoader, TranslateFakeLoader } from '@ngx-translate/core';
+import {DpDatePickerModule} from 'ng2-date-picker';
+import * as moment from 'moment';
 
 describe('Date-range-selector component', () => {
     let component, fixture;
@@ -22,7 +23,7 @@ describe('Date-range-selector component', () => {
             imports: [
                 TranslateModule.forRoot({
                     loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
-                }),
+                }), DpDatePickerModule
             ],
             providers: [
                 ApiService,
@@ -52,6 +53,12 @@ describe('Date-range-selector component', () => {
         expect(fixture.debugElement.query(By.css("button[aria-label='prev']"))).not.toBeNull();
     }));
 
+    it('has a date input', async(() => {
+        const input = fixture.debugElement.query(By.css("input[type='text']"));
+        console.log(fixture.debugElement.query(By.all()).nativeElement);
+        expect(input).not.toBeNull();
+    }));
+
     it('has a next button', async(() => {
         expect(fixture.debugElement.query(By.css("button[aria-label='next']"))).not.toBeNull();
     }));
@@ -74,5 +81,82 @@ describe('Date-range-selector component', () => {
     it('has a day-range button', async(() => {
         const buttonsDate = fixture.debugElement.queryAll(By.css("div.buttons > button"));
         expect(buttonsDate.filter(button => button.nativeElement.innerHTML.includes("control.date-range")).length).toBe(1);
+    }));
+
+    it('date range calendar is popup', async(() => {
+        const buttonsDate = fixture.debugElement.queryAll(By.css("div.buttons > button"));
+        let btn = buttonsDate.filter(button => button.nativeElement.innerHTML.includes("control.date-range")).shift();
+        let clickEvent = btn.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        const input = fixture.debugElement.query(By.css("input[type='text']"));
+        clickEvent = input.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();        
+        fixture.detectChanges();
+        console.log(fixture.debugElement.query(By.css("*")).nativeElement);
+        expect(fixture.debugElement.query(By.css("div[data-hidden='false'].popup"))).not.toBeNull();
+    }));
+
+    it('upper limit in the calendar is today', async(() => {
+        const buttonsDate = fixture.debugElement.queryAll(By.css("div.buttons > button"));
+        let btn = buttonsDate.filter(button => button.nativeElement.innerHTML.includes("control.date-range")).shift();
+        let clickEvent = btn.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        const input = fixture.debugElement.query(By.css("input[type='text']"));
+        clickEvent = input.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css("button.dp-current-day")).nativeElement.dataset.date)
+        .toBe(moment().format('YYYY-MM-DD'));
+    }));
+
+    it('upper limit in the calendar is today', async(() => {
+        const buttonsDate = fixture.debugElement.queryAll(By.css("div.buttons > button"));
+        let btn = buttonsDate.filter(button => button.nativeElement.innerHTML.includes("control.date-range")).shift();
+        let clickEvent = btn.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        const input = fixture.debugElement.query(By.css("input[type='text']"));
+        clickEvent = input.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css("button.dp-current-day")).nativeElement.dataset.date)
+        .toBe(moment().format('YYYY-MM-DD'));
+    }));
+
+    it('calendar date buttons is clickable', async(() => {
+        const buttonsDate = fixture.debugElement.queryAll(By.css("div.buttons > button"));
+        let btn = buttonsDate.filter(button => button.nativeElement.innerHTML.includes("control.date-range")).shift();
+        let clickEvent = btn.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        const input = fixture.debugElement.query(By.css("input[type='text']"));
+        clickEvent = input.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        fixture.detectChanges();
+        let tomorrow = moment().add(1, 'day');
+        let buttonTomorrowDate = fixture.debugElement.query(By.css(`button[data-date='${tomorrow.format('YYYY-MM-DD')}']`));
+        clickEvent = buttonTomorrowDate.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        fixture.detectChanges();
+        console.log(fixture.debugElement.query(By.css("button.dp-selected")).nativeElement);
+        expect(fixture.debugElement.query(By.css("button.dp-selected")).nativeElement.dataset.date)
+        .toBe(tomorrow.format('YYYY-MM-DD'));
+    }));
+
+    it('far dates is not clickable', async(() => {
+        const buttonsDate = fixture.debugElement.queryAll(By.css("div.buttons > button"));
+        let btn = buttonsDate.filter(button => button.nativeElement.innerHTML.includes("control.date-range")).shift();
+        let clickEvent = btn.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        const input = fixture.debugElement.query(By.css("input[type='text']"));
+        clickEvent = input.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        fixture.detectChanges();
+        let afterTomorrow = moment().add(2, 'day');
+        let buttonTomorrowDate = fixture.debugElement
+        .query(By.css(`button[data-date='${afterTomorrow.format('YYYY-MM-DD')}']`));
+        clickEvent = buttonTomorrowDate.listeners.filter(event => event.name == 'click').shift();
+        clickEvent.callback.call();
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css("button.dp-selected[disabled]")).nativeElement.dataset.date)
+        .toBe(afterTomorrow.format('YYYY-MM-DD'));
     }));
 });
