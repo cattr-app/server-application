1cbdb93c3acd257666c29953d6498421aaf6880a
Alexander Yanchuk <alexanderyanchuk95@gmail.com>
2018-11-12 18:18:54 +0600

    Remove index on registration email

--- database/migrations/2018_11_02_121027_create_registrations_table.php
+++ database/migrations/2018_11_02_121027_create_registrations_table.php
@@ -19,7 +19,7 @@ class CreateRegistrationsTable extends Migration
             Schema::create('registrations', function (Blueprint $table) {
                 $table->increments('id');
                 $table->uuid('key')->unique();
-                $table->string('email')->unique();
+                $table->string('email');
                 $table->dateTime('expires_at');
             });
         }
