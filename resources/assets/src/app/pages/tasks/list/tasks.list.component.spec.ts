e2b93e39965a1e857b10512a4037620dd051706b
Vycheslav Sokolov <vycheslavsokolov2013@gmail.com>
2018-11-19 08:36:16 +0600

    Added checking buttons delete/edit/show for task list component. Overidded reload() for task list mock component.

--- resources/assets/src/app/pages/tasks/list/tasks.list.component.spec.ts
+++ resources/assets/src/app/pages/tasks/list/tasks.list.component.spec.ts
@@ -13,9 +13,54 @@ import { loadAdminStorage, loadManagerStorage, loadUserStorage } from '../../../
 import { BsModalService, ComponentLoaderFactory, PositioningService } from 'ngx-bootstrap';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { ProjectsService } from '../../projects/projects.service';
+import { Task } from '../../../models/task.model';
 
 class TaskListComponentMock extends TasksListComponent {
+  
+  tasks: Task[] = [
+    {
+    'id': 1,
+    'project_id': 1,
+    'task_name': 'Task name #1',
+    'description': 'description #1',
+    'deleted_at': null,
+    'created_at': '2018-09-03 02:10:51',
+    'updated_at': '2018-09-03 02:10:51'
+  },
 
+  {
+    'id': 2,
+    'project_id': 1,
+    'task_name': 'Task name #2',
+    'description': 'description #2',
+    'deleted_at': null,
+    'created_at': '2018-09-03 02:10:31',
+    'updated_at': '2018-09-03 02:10:31'
+  },
+
+  {
+    'id': 3,
+    'project_id': 1,
+    'task_name': 'Task name #3',
+    'description': 'description #3',
+    'deleted_at': null,
+    'created_at': '2018-09-03 02:10:41',
+    'updated_at': '2018-09-03 02:10:41'
+  }
+]
+
+  reload() {
+    this.setItems(this.tasks);
+    this.setDirectProjects();
+  }
+
+  setDirectProjects() {
+    let set = new Set();
+    this.tasks.forEach(task => {
+      set.add(task.project_id);
+    });
+    this.directProject = Array.from(set);
+  }
 }
 
 describe('Tasks list component(Admin)', () => {
@@ -49,6 +94,7 @@ describe('Tasks list component(Admin)', () => {
         loadAdminStorage();
         fixture = TestBed.createComponent(TaskListComponentMock);
         component = fixture.debugElement.componentInstance;
+        component.reload();
         fixture.detectChanges();
       });
   }));
@@ -66,7 +112,28 @@ describe('Tasks list component(Admin)', () => {
   }));
 
   it('has button add new task', async(() => {
-    expect(fixture.debugElement.query(By.css("a[title='Add new task']"))).not.toBeNull();
+    expect(fixture.debugElement.query(By.css("a[title='Add new task'"))).not.toBeNull();
+  }));
+
+  it('has button view task', async(() => {
+    let infoButtons = fixture.debugElement.queryAll(By.css("button.btn-info"));
+    expect(infoButtons).not.toBeNull();
+    let index = Math.floor(Math.random() * component.tasks.length);
+    expect(infoButtons[index].nativeElement.innerHTML).toContain("control.view");
+  }));
+
+  it('has button edit task', async(() => {
+    let editButtons = fixture.debugElement.queryAll(By.css("button.btn-primary"));
+    expect(editButtons).not.toBeNull();
+    let index = Math.floor(Math.random() * component.tasks.length);
+    expect(editButtons[index].nativeElement.innerHTML).toContain("control.edit");
+  }));
+
+  it('has button delete task', async(() => {
+    let delButtons = fixture.debugElement.queryAll(By.css("button.btn-danger"));
+    expect(delButtons).not.toBeNull();
+    let index = Math.floor(Math.random() * component.tasks.length);
+    expect(delButtons[index].nativeElement.innerHTML).toContain("control.delete");
   }));
 });
 
@@ -120,6 +187,23 @@ describe('Tasks list component(Manager)', () => {
   it('has not button add new task', async(() => {
     expect(fixture.debugElement.query(By.css("a[title='Add new task']"))).toBeNull();
   }));
+
+  it('has button view task', async(() => {
+    let infoButtons = fixture.debugElement.queryAll(By.css("button.btn-info"));
+    expect(infoButtons).not.toBeNull();
+    let index = Math.floor(Math.random() * component.tasks.length);
+    expect(infoButtons[index].nativeElement.innerHTML).toContain("control.view");
+  }));
+
+  it('has button edit task', async(() => {
+    let editButtons = fixture.debugElement.queryAll(By.css("button.btn-primary"));
+    expect(editButtons).not.toBe([]);
+  }));
+
+  it('has button delete task', async(() => {
+    let delButtons = fixture.debugElement.queryAll(By.css("button.btn-danger"));
+    expect(delButtons).not.toBe([]);
+  }));
 });
 
 describe('Tasks list component(User)', () => {
@@ -171,5 +255,26 @@ describe('Tasks list component(User)', () => {
 
   it('has button add new task', async(() => {
     expect(fixture.debugElement.query(By.css("a[title='Add new task']"))).not.toBeNull();
+  }));
+
+  it('has button view task', async(() => {
+    let infoButtons = fixture.debugElement.queryAll(By.css("button.btn-info"));
+    expect(infoButtons).not.toBeNull();
+    let index = Math.floor(Math.random() * component.tasks.length);
+    expect(infoButtons[index].nativeElement.innerHTML).toContain("control.view");
+  }));
+
+  it('has button edit task', async(() => {
+    let editButtons = fixture.debugElement.queryAll(By.css("button.btn-primary"));
+    expect(editButtons).not.toBeNull();
+    let index = Math.floor(Math.random() * component.tasks.length);
+    expect(editButtons[index].nativeElement.innerHTML).toContain("control.edit");
+  }));
+
+  it('has button delete task', async(() => {
+    let delButtons = fixture.debugElement.queryAll(By.css("button.btn-danger"));
+    expect(delButtons).not.toBeNull();
+    let index = Math.floor(Math.random() * component.tasks.length);
+    expect(delButtons[index].nativeElement.innerHTML).toContain("control.delete");
   }));
 });
