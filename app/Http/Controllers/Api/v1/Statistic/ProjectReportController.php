eac21363f813e4a31f9939ede2af283065f5f404
Vladimir Shchipanov <mohax.mon@gmail.com>
2019-03-21 17:58:38 +0600

    Fix project report filter

--- app/Http/Controllers/Api/v1/Statistic/ProjectReportController.php
+++ app/Http/Controllers/Api/v1/Statistic/ProjectReportController.php
@@ -43,7 +43,7 @@ class ProjectReportController extends Controller
             ->whereIn('user_id', $uids)
             ->whereIn('project_id', $pids)
             ->where('date', '>=', $start_at)
-            ->where('date', '<', $end_at)
+            ->where('date', '<=', $end_at)
             ->groupBy('user_id', 'user_name', 'task_id', 'project_id', 'task_name', 'project_name')
             ->get();
 
