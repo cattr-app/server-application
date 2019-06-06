33b94823276363b570c421389d77b594bf733e1b
Matrix <matrix.aas@gmail.com>
2019-06-06 16:19:37 +0700

    Remove unnecessary files

--- Modules/RedmineIntegration/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('redmineintegration::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('redmineintegration.name') !!}
-    </p>
-@stop
