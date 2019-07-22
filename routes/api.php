b8e7c26cd3a4fb61da04b12d93999095e15c8885
Matrix <matrix.aas@gmail.com>
2019-07-22 19:26:45 +0700

    Delete assign-users feature

--- routes/api.php
+++ routes/api.php
@@ -106,14 +106,6 @@ Route::group([
     $router->post('/users/bulk-edit', 'Api\v1\UserController@bulkEdit');
     $router->any('/users/relations', 'Api\v1\UserController@relations');
 
-    // Attached Users routes
-    $router->any('/attached-users/list', 'Api\v1\RelationsUsersController@index');
-    $router->any('/attached-users/count', 'Api\v1\RelationsUsersController@count');
-    $router->post('/attached-users/create', 'Api\v1\RelationsUsersController@create');
-    $router->post('/attached-users/bulk-create', 'Api\v1\RelationsUsersController@bulkCreate');
-    $router->post('/attached-users/remove', 'Api\v1\RelationsUsersController@destroy');
-    $router->post('/attached-users/bulk-remove', 'Api\v1\RelationsUsersController@bulkDestroy');
-
     //Screenshots routes
     $router->any('/screenshots/list', 'Api\v1\ScreenshotController@index');
     $router->any('/screenshots/count', 'Api\v1\ScreenshotController@count');
