32f625a510c31432d4c88528c198cd84509e0fa1
perkthefox <antonkevinok@gmail.com>
2019-08-21 22:21:57 +0700

    Add websocket

---
+++ Modules/RedmineIntegration/Events/TaskReceived.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\RedmineIntegration\Events;
+
+use App\Models\Task;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TaskReceived implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var Task
+     */
+    protected $task;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Task $task
+     */
+    public function __construct($task)
+    {
+        $this->task = $task;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return PrivateChannel
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel("task.updates.{$this->task->user_id}");
+    }
+}
