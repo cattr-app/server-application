2002968109669e0386b14d4a2953e7c65fbd8e3a
zero <masterzerno@gmail.com>
2019-10-09 20:19:24 +0600

    add CORS headers in OPTIONS method

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,11 +15,24 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $this->allowAllCors();
+
+        return $next($request);
+    }
+
+
+    public function terminate($request, $response)
+    {
+        if ($request->getMethod() == 'OPTIONS') {
+            $this->allowAllCors();
+        }
+    }
+
+    protected function allowAllCors()
+    {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
         header('Access-Control-Allow-Headers: *');
         header('Access-Control-Max-Age: 1728000');
-
-        return $next($request);
     }
 }
