43da2c7bd7f76c1b825a683abd729ca074fa5280
zero <masterzerno@gmail.com>
2019-10-10 18:44:05 +0600

    add trusted domains to .env file

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -6,6 +6,24 @@ use Closure;
 
 class CorsMiddleware
 {
+
+    protected $trustedDomains = [];
+
+
+    public function __construct()
+    {
+        // get domain list in "domain1.com, domain2.com, domain3.com, domainN.com"
+        $domains = config('cors.trustedDomains');
+
+        // remove spaces
+        $domains = str_replace([' '],'', $domains);
+
+        if ($domains) {
+            // string => array ['domain1.com', 'domain2.com', 'domain3.com', 'domainN.com']
+            $this->trustedDomains = explode(',', $domains);
+        }
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -15,8 +33,7 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $this->allowAllCors();
-
+        $this->allowAllCors($request);
         return $next($request);
     }
 
@@ -24,13 +41,35 @@ class CorsMiddleware
     public function terminate($request, $response)
     {
         if ($request->getMethod() == 'OPTIONS') {
-            $this->allowAllCors();
+            $this->allowAllCors($request);
         }
     }
 
-    protected function allowAllCors()
+    protected function allowAllCors($request)
     {
-        header('Access-Control-Allow-Origin: *');
+        $origin = $request->header('Origin');
+
+        if (is_null($origin)) {
+            return;
+        }
+
+        $requestDomain = parse_url($origin, PHP_URL_HOST);
+        $requestPort = parse_url($origin, PHP_URL_PORT);
+
+        if (is_null($requestDomain)) {
+            return;
+        }
+
+        // has port? add it to domain
+        if (!is_null($requestPort)) {
+            $requestDomain = $requestDomain . ':' . $requestPort;
+        }
+
+        if (!in_array($requestDomain, $this->trustedDomains)) {
+            return;
+        }
+
+        header('Access-Control-Allow-Origin: ' . $requestDomain);
         header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
         header('Access-Control-Allow-Headers: *');
         header('Access-Control-Max-Age: 1728000');
