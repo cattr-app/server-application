56d7746727bdde37d1b8c356298bdc147448ab31
zero <masterzerno@gmail.com>
2019-10-10 17:12:29 +0600

    hotfix sentry exception sending

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -33,7 +33,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-        if (config('app.debug') && app()->bound('sentry') && $this->shouldReport($exception)){
+        if (!config('app.debug') && app()->bound('sentry') && $this->shouldReport($exception)){
             app('sentry')->captureException($exception);
         }
 
