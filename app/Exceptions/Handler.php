31302ef32288e0af632e9c70a17790ce2ebd1a7d
perkthefox <antonkevinok@gmail.com>
2019-10-18 15:26:44 +0700

    Fix Exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -23,11 +23,13 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         AuthenticationException::class,
         AuthorizationException::class,
+        Entities\AuthorizationException::class,
         HttpException::class,
         ModelNotFoundException::class,
         TokenMismatchException::class,
         ValidationException::class,
     ];
+
 
     /**
      * Report or log an exception.
@@ -58,47 +60,61 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        // If current request is an XMLHttpRequest or it has an "Accept: application/json" header we'll assume that
-        // this request was sent trough client or frontend application
-        if ($request->wantsJson() || $request->ajax()) {
-            try {
-                $message = is_object($exception->getMessage()) ? $exception->getMessage()->toArray() : $exception->getMessage();
-            } catch (Exception $e) {
-                // If exception message is an object but it does not have a toArray() method we'll processing it as a
-                // plain object
-                $message = $exception->getMessage();
-            }
+        if (!config('app.json_errors')) {
+            return parent::render($request, $exception);
+        }
 
-            if (config('app.debug')) {
+        $message = is_object($exception->getMessage()) ? $exception->getMessage()->toArray() : $exception->getMessage();
+
+        if (config('app.debug')) {
+            $validExceptionsClasses = array_map(function ($item) use ($exception) {
+                return is_subclass_of($exception, $item);
+            }, $this->dontReport);
+
+            if (!$this->isHttpException($exception) && empty($validExceptionsClasses)) {
                 $debugData = [
                     'file' => $exception->getFile(),
                     'line' => $exception->getLine(),
                     'code' => $exception->getCode(),
-                    'trace' => $exception->getTrace(),
+                    'class' => get_class($exception),
+                    'trace' => $exception->getTrace()
                 ];
             }
-
-            // Processing exception status code
-            if ($exception instanceof \Error) {
-                // If current exception is an PHP default error we'll interpret it as 500 Server Error code
-                $statusCode = 500;
-            } elseif ($this->isHttpException($exception)) {
-                // Otherwise, if it is Laravel's HttpException we can access getStatusCode() method from exception
-                // instance
-                $statusCode = $exception->getStatusCode();
-            } else {
-                // Otherwise, if non of previous checks was correct we'll assuming that current exception was thrown
-                // because of a bad request body
-                $statusCode = 400;
-            }
-
-            // Debug data will be passed to response body only if application currently in debug mode
-            return response()->json(
-                array_merge(['message' => $message], isset($debugData) ? ['debug' => $debugData] : []), $statusCode
-            );
         }
 
-        return parent::render($request, $exception);
+        // Processing exception status code
+        if ($exception instanceof \Error) {
+            // If current exception is an PHP default error we'll interpret it as 500 Server Error code
+            $statusCode = 500;
+        } elseif ($this->isHttpException($exception)) {
+            // Otherwise, if it is Laravel's HttpException we can access getStatusCode() method from exception
+            // instance
+            $statusCode = $exception->getStatusCode();
+            if ($statusCode == 404) {
+                $message = __("Requested url :url was not found", ['url' => $request->getRequestUri()]);
+            }
+        } elseif ($exception->getCode() == 404 || $exception->getCode() == 401) {
+            // If we have 404 or 401 code we will process it as an request status code
+            $statusCode = $exception->getCode();
+        } else {
+            // Otherwise, if non of previous checks was correct we'll assuming that current exception was thrown
+            // because of a bad request body
+            $statusCode = 400;
+        }
+
+        if ($exception instanceof Entities\AuthorizationException) {
+            $authReason = $exception->getReason();
+        }
+
+        // Debug data will be passed to response body only if application currently in debug mode
+        return response()->json(
+            array_merge(
+                ['message' => $message],
+                isset($debugData) ? ['debug' => $debugData] : [],
+                isset($authReason) ? ['reason' => $authReason] : []
+            ),
+            $statusCode
+        );
     }
 
     /**
