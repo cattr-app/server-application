ea070b5ffb867794462f0ef40d0527239e21421b
Vladislav Matyash <matyashvd@gmail.com>
2019-12-03 20:16:33 +0600

    Password reset rework

--- app/User.php
+++ app/User.php
@@ -241,6 +241,19 @@ class User extends Authenticatable implements JWTSubject, CanResetPassword
     {
         return $this->hasMany(Token::class);
     }
+
+    /**
+     * @param $token
+     * @return Token
+     */
+    public function addToken($token)
+    {
+        $tokenExpires = date('Y-m-d H:i:s', time() + 60 * auth()->factory()->getTTL());
+        /** @var Token $token */
+        $token = $this->tokens()->create(['token' => $token, 'expires_at' => $tokenExpires]);
+        return $token;
+
+    }
     /**
      * @return HasMany
      */
