5a1fc09de47680358f7ad6e8dc6dcf83a0722aaa
Vladislav Matyash <matyashvd@gmail.com>
2019-12-11 19:35:36 +0600

    Fix ApiDoc

--- app/Exceptions/Entities/AuthorizationException.php
+++ app/Exceptions/Entities/AuthorizationException.php
@@ -133,7 +133,7 @@ class AuthorizationException extends BaseAuthorizationException
     /**
      * @apiDefine ForbiddenError
      * @apiErrorExample {json} Forbidden
-     *  HTTP/1.1 401 Forbidden
+     *  HTTP/1.1 403 Forbidden
      *  {
      *    "success": false,
      *    "message": "Forbidden"
