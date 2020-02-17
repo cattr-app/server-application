0528b3f4b40bfaf2ef324c48d9a7ce75868edc0c
Alexander Yanchuk <alexander@amazingcat.net>
2020-02-17 12:28:34 +0600

    Fix deleted intervals appearing in dashboard export

--- Modules/Reports/Entities/DashboardReport.php
+++ Modules/Reports/Entities/DashboardReport.php
@@ -7,9 +7,12 @@ namespace Modules\Reports\Entities;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DashboardReport extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'time_intervals';
 
     /**
