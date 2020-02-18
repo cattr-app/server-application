9fc2060890027dd047b52015c55b44acc883e606
Vladislav Matyash <matyashvd@gmail.com>
2020-02-18 12:39:18 +0600

    Add DoesNotExist rule

---
+++ app/Rules/DoesNotExist.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+
+class DoesNotExist implements Rule
+{
+    /**
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @param string $table
+     * @param int $id
+     */
+    public function __construct(string $table, int $id)
+    {
+        $this->table = $table;
+        $this->id = $id;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        return DB::table($this->table)
+            ->where($this->id, $value)
+            ->doesntExist();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'Item already exists';
+    }
+}
