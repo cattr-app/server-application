116dbe5c36ba4d2454fd86d0febb5b6e279b0790
Alexander Yanchuk <alexander@amazingcat.net>
2020-02-18 16:47:07 +0600

    Recreate roles

--- database/migrations/2020_02_12_134247_recreate_roles.php
+++ database/migrations/2020_02_12_134247_recreate_roles.php
@@ -13,11 +13,11 @@ class RecreateRoles extends Migration
      */
     public function up()
     {
-        DB::raw('SET FOREIGN_KEY_CHECKS = 0');
+        /*DB::raw('SET FOREIGN_KEY_CHECKS = 0');
         DB::raw('TRUNCATE role');
         DB::raw('TRUNCATE rule');
         DB::raw('SET FOREIGN_KEY_CHECKS = 1');
-        Artisan::call('db:seed', ['--class' => 'RoleSeeder']);
+        Artisan::call('db:seed', ['--class' => 'RoleSeeder']);*/
     }
 
     /**
