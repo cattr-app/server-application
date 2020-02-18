7366f7c9b1d8336326a6e5f65cf0c1f7e85349a6
Alexander Yanchuk <alexander@amazingcat.net>
2020-02-18 18:27:37 +0600

    Add task edit/remove rules to the user role

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -35,6 +35,8 @@ class RoleSeeder extends Seeder
             ],
             'tasks' => [
                 'create',
+                'edit',
+                'remove',
             ],
             'time' => [
                 'project',
