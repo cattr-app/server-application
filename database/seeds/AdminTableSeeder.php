d88c8fbb8668be061a5b408d4c618997252bc696
Alex Vyarvelsky <alex.vyarvelsky@amazingcat.net>
2020-02-25 17:59:33 +0700

    Admin table seeder fix: a new admin will be created only if there are no other admins

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Seeder;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 
+/**
+ * Class AdminTableSeeder
+ */
 class AdminTableSeeder extends Seeder
 {
     /**
@@ -13,35 +16,37 @@ class AdminTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->command->getOutput()->writeln('<fg=yellow>Create admin user</>');
-
         $login = 'admin@example.com';
         $pass = 'admin';
 
-        /** @var User $admin */
-        $admin = User::query()->updateOrCreate([
-            'full_name'              => 'Admin',
-            'email'                  => $login,
-            'url'                    => '',
-            'company_id'             => 1,
-            'payroll_access'         => 1,
-            'billing_access'         => 1,
-            'avatar'                 => '',
-            'screenshots_active'     => 1,
-            'manual_time'            => 0,
-            'permanent_tasks'        => 0,
-            'computer_time_popup'    => 300,
-            'poor_time_popup'        => '',
-            'blur_screenshots'       => 0,
-            'web_and_app_monitoring' => 1,
-            'webcam_shots'           => 0,
-            'screenshots_interval'   => 9,
-            'active'                 => true,
-            'password'               => bcrypt($pass),
-            'is_admin'               => true,
-            'role_id'                => 2,
-        ]);
+        if (!User::where('is_active', true)->exists()) {
+            $this->command->getOutput()->writeln('<fg=yellow>Create admin user</>');
 
-        $this->command->getOutput()->writeln('<fg=green>Admin user has been created</>');
+            /** @var User $admin */
+            $admin = User::query()->updateOrCreate([
+                'full_name' => 'Admin',
+                'email' => $login,
+                'url' => '',
+                'company_id' => 1,
+                'payroll_access' => 1,
+                'billing_access' => 1,
+                'avatar' => '',
+                'screenshots_active' => 1,
+                'manual_time' => 0,
+                'permanent_tasks' => 0,
+                'computer_time_popup' => 300,
+                'poor_time_popup' => '',
+                'blur_screenshots' => 0,
+                'web_and_app_monitoring' => 1,
+                'webcam_shots' => 0,
+                'screenshots_interval' => 9,
+                'active' => true,
+                'password' => bcrypt($pass),
+                'is_admin' => true,
+                'role_id' => 2,
+            ]);
+
+            $this->command->getOutput()->writeln('<fg=green>Admin user has been created</>');
+        }
     }
 }
