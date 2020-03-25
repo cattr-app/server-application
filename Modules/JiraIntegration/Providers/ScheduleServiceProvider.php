6dde187e9c0a00a366d360aa2cc2ff3448bdfb9c
Vitaliy Pavkin <vitaly@amazingcat.net>
2020-03-25 08:03:06 +0100

    Resolve "Cron for gitlab not working"

--- Modules/JiraIntegration/Providers/ScheduleServiceProvider.php
+++ Modules/JiraIntegration/Providers/ScheduleServiceProvider.php
@@ -10,7 +10,7 @@ class ScheduleServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->app->booted(function () {
-            $schedule = $this->app->make(Schedule::class);
+            $schedule = app(Schedule::class);
             $schedule->command('jira:sync-tasks')->everyFiveMinutes()->withoutOverlapping();
             $schedule->command('jira:sync-time')->everyMinute()->withoutOverlapping();
         });
