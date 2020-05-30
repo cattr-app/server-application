8d47c324c02609952b7a6ad8a360e3ee5e0b28b6
Alex Vyarvelsky <alex@cattr.app>
2020-05-30 16:59:13 +0700

    chore: make routes cacheable

--- routes/api.php
+++ routes/api.php
@@ -58,8 +58,7 @@ Route::group([
     $router->get('me', 'AuthController@me');
     $router->post('password/reset/request', 'PasswordResetController@request');
     $router->post('password/reset/validate', 'PasswordResetController@validate');
-    $router->post('password/reset/process', 'PasswordResetController@process')
-        ->name('password.reset.process');
+    $router->post('password/reset/process', 'PasswordResetController@process');
 
     $router->get('/register/{key}', 'RegistrationController@getForm');
     $router->post('/register/{key}', 'RegistrationController@postForm');
@@ -77,8 +76,7 @@ Route::group([
     $router->get('me', 'AuthController@me');
     $router->post('password/reset/request', 'PasswordResetController@request');
     $router->post('password/reset/validate', 'PasswordResetController@validate');
-    $router->post('password/reset/process', 'PasswordResetController@process')
-        ->name('password.reset.process');
+    $router->post('password/reset/process', 'PasswordResetController@process');
 
     $router->get('/register/{key}', 'RegistrationController@getForm');
     $router->post('/register/{key}', 'RegistrationController@postForm');
@@ -196,40 +194,4 @@ Route::group([
     $router->post('/companymanagement/language/edit', 'Api\v1\CompanyManagementController@editLanguage');
 });
 
-// Laravel router pass to fallback not non-exist urls only but wrong-method requests too.
-// So required to check if route have alternative request methods
-// throw not-found or wrong-method exceptions manually
-Route::any('(.*)', static function () {
-
-    /** @var Router $router */
-    $router = app('router');
-    /** @var Request $request */
-    $request = $router->getCurrentRequest();
-    /** @var RouteCollection $routes */
-    $routeCollection = $router->getRoutes();
-    /** @var string[] $methods */
-    $methods = array_diff(Router::$verbs, [$request->getMethod(), 'OPTIONS']);
-
-    foreach ($methods as $method) {
-        // Get all routes for method without fallback routes
-        /** @var Route[]|Collection $routes */
-        $routes = collect($routeCollection->get($method))->filter(static function ($route) {
-            /** @var RouteModel $route */
-            return !$route->isFallback && $route->uri !== '{fallbackPlaceholder}';
-        });
-
-        // Look if any route have match with current request
-        $mismatch = $routes->first(static function ($value) use ($request) {
-            /** @var RouteModel $value */
-            return $value->matches($request, false);
-        });
-
-        // Throw wrong-method exception if matches found
-        if ($mismatch !== null) {
-            throw new MethodNotAllowedHttpException([]);
-        }
-    }
-
-    // No matches, throw not-found exception
-    throw new NotFoundHttpException();
-});
+Route::any('(.*)', 'Controller@universalRoute');
