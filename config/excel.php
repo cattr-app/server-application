2940d8dbaff3750d1b403e4120ec7ba2df7258a9
Alex Vyarvelsky <alex@cattr.app>
2020-06-19 21:12:31 +0700

    chore: change default tmp path for excelexporter

--- config/excel.php
+++ config/excel.php
@@ -163,7 +163,7 @@ return [
         | storing reading or downloading. Here you can customize that path.
         |
         */
-        'local_path' => storage_path('framework/tmp/exporter'),
+        'local_path' => storage_path('framework/tmp'),
 
         /*
         |--------------------------------------------------------------------------
