fe9d4b7c6c7450e2d68477acff54214c60fceca2
Vitaliy Pavkin <vitaly@amazingcat.net>
2020-07-08 20:54:44 +0700

    feat: add activity properties to time intervals

---
+++ app/Http/Requests/v1/TimeInterval/CreateTimeIntervalRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\v1\TimeInterval;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTimeIntervalRequest extends FormRequest
+{
+    /**
+     * Determine if user authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'task_id' => 'required|exists:tasks,id',
+            'user_id' => 'required|exists:users,id',
+            'start_at' => 'required|date',
+            'end_at' => 'required|date',
+            'activity_fill' => 'int|between:0,100',
+            'mouse_fill' => 'int|between:0,100',
+            'keyboard_fill' => 'int|between:0,100',
+            'is_manual' => 'bool',
+        ];
+    }
+}
