c0c0fd7c9fc29848ce2fb242aafcb3a3bb3fe483
Daniyar Adilov <daniyar@amazingcat.net>
2020-11-05 06:17:39 +0100

    Resolve "Display the source in the project name"

--- tests/Factories/ProjectFactory.php
+++ tests/Factories/ProjectFactory.php
@@ -67,6 +67,7 @@ class ProjectFactory extends Factory
             'company_id' => self::COMPANY_ID,
             'name' => $faker->company,
             'description' => $faker->text(self::DESCRIPTION_LENGTH),
+            'source' => 'internal',
         ];
     }
 
