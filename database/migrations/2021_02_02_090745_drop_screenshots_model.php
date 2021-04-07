004452510d16ad5c9d1116ba1c2a974f0428dad0
Alexander Yanchuk <alexander@amazingcat.net>
2021-04-07 10:22:55 +0600

    fix: screenshots migration broken after removing screenshot model

--- database/migrations/2021_02_02_090745_drop_screenshots_model.php
+++ database/migrations/2021_02_02_090745_drop_screenshots_model.php
@@ -18,17 +18,19 @@ class DropScreenshotsModel extends Migration
             Storage::makeDirectory('screenshots/thumbs');
         }
 
-        foreach (Screenshot::cursor() as $screenshot) {
-            $fileName = hash('sha256', $screenshot->time_interval_id) . '.jpg';
+        DB::table('screenshots')->orderBy('id')->chunk(1000, static function ($screenshots) {
+            foreach ($screenshots as $screenshot) {
+                $fileName = hash('sha256', $screenshot->time_interval_id) . '.jpg';
 
-            if (Storage::exists($screenshot->path)) {
-                Storage::move($screenshot->path, 'screenshots/' . $fileName);
-            }
+                if (Storage::exists($screenshot->path)) {
+                    Storage::move($screenshot->path, 'screenshots/' . $fileName);
+                }
 
-            if (Storage::exists($screenshot->thumbnail_path)) {
-                Storage::move($screenshot->thumbnail_path, 'screenshots/thumbs/' . $fileName);
+                if (Storage::exists($screenshot->thumbnail_path)) {
+                    Storage::move($screenshot->thumbnail_path, 'screenshots/thumbs/' . $fileName);
+                }
             }
-        }
+        });
 
         Storage::deleteDirectory('uploads/screenshots');
 
