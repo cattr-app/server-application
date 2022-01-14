28150d65931b32e2b9e949deb0ba6acbf127211e
Alex Vyarvelsky <vermorag@wolfin.net>
2022-01-14 12:18:20 +0300

    chore: move EventServiceProvider to Providers folder

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    protected $listen = [];
+
+    protected $subscribe = [];
+
+    public function shouldDiscoverEvents(): bool
+    {
+        return true;
+    }
+}
