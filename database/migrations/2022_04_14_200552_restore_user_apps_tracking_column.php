5638cb6e24b70992dd8db109f7860cd1d7e417bc
Alex V <alex@cattr.app>
2022-04-15 03:57:33 +0700

    feat: change user app tracking property to bool

---
+++ database/migrations/2022_04_14_200552_restore_user_apps_tracking_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->boolean('web_and_app_monitoring')->default(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->integer('web_and_app_monitoring')->change();
+        });
+    }
+};
