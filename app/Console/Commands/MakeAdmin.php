ee257a9f456cee40a7b2955baa15ffa6b66fd437
AlexV <alex@amazingcat.net>
2023-03-10 20:05:56 +0100

    fix: make admin command

--- app/Console/Commands/MakeAdmin.php
+++ app/Console/Commands/MakeAdmin.php
@@ -6,16 +6,21 @@ use App\Models\User;
 use Illuminate\Console\Command;
 use Validator;
 use Illuminate\Validation\Rule;
+use Illuminate\Contracts\Console\Isolatable;
 
-class MakeAdmin extends Command
+class MakeAdmin extends Command implements Isolatable
 {
-    protected $signature = 'cattr:make:admin {--o : Skip instance registration}';
+    protected $signature = 'cattr:make:admin
+                            {--o : Skip instance registration}
+                            {--e|email : user email}
+                            {--n|name : user name}
+                            {--p|password : user password}';
     protected $description = 'Creates admin user';
 
     public function handle(): void
     {
         do {
-            $email = $this->ask('What is your email?', 'admin@example.com');
+            $email = $this->option('email') ?: $this->ask('What is your email?', 'admin@example.com');
 
             $validator = Validator::make([
                 'email' => $email
@@ -44,7 +49,7 @@ class MakeAdmin extends Command
         }
 
         do {
-            $password = $this->secret('What password should we set?');
+            $password = $this->option('password') ?: $this->secret('What password should we set?');
 
             $validator = Validator::make([
                 'password' => $password
@@ -60,7 +65,7 @@ class MakeAdmin extends Command
         } while (!$passwordIsValid);
 
         User::factory()->admin()->create([
-            'full_name' => $this->ask('What is your name?', 'Admin'),
+            'full_name' => $this->option('name') ?: $this->ask('What is your name?', 'Admin'),
             'email' => $email,
             'password' => $password,
             'last_activity' => now(),
