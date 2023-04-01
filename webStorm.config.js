ebf79e6cdb6ab7209ae508b093e985c588d918c7
AlexV <alex@amazingcat.net>
2023-04-01 02:15:59 +0200

    feat: add webStorm config
    Signed-off-by: AlexV <alex@amazingcat.net>

---
+++ webStorm.config.js
@@ -0,0 +1,8 @@
+System.config({
+  "paths": {
+    "@/*": "resources/frontend/core/*",
+    "_app/*": "resources/frontend/*",
+    "_modules/*": "resources/frontend/vendor_modules/*",
+    "_internal/*": "resources/frontend/core/modules/*",
+  }
+});
