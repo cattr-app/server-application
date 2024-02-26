cceefe5414c192f824e577705e98ea3c7c97ed16
OlegDenisov95 <hanonhanon95@gmail.com>
2024-02-26 20:16:43 +0600

    fix: change timelinecalendar

--- resources/frontend/core/modules/Dashboard/components/TimelineCalendarGraph.vue
+++ resources/frontend/core/modules/Dashboard/components/TimelineCalendarGraph.vue
@@ -1,26 +1,12 @@
 <template>
-    <div ref="canvasWrapper" class="canvas">
-        <canvas ref="canvas" />
-    </div>
+    <div ref="canvas" class="canvas"></div>
 </template>
 
 <script>
-    import { fabric } from 'fabric';
     import throttle from 'lodash/throttle';
     import moment from 'moment';
     import { formatDurationString } from '@/utils/time';
-
-    const fabricObjectOptions = {
-        editable: false,
-        selectable: false,
-        objectCaching: false,
-        hasBorders: false,
-        hasControls: false,
-        hasRotatingPoint: false,
-        cursor: 'default',
-        hoverCursor: 'default',
-    };
-
+    import { SVG } from '@svgdotjs/svg.js';
     const headerHeight = 20;
     const columns = 7;
     const rowHeight = 120;
@@ -42,211 +28,148 @@
             },
         },
         mounted() {
-            this.canvas = new fabric.Canvas(this.$refs.canvas, {
-                backgroundColor: '#fff',
-                renderOnAddRemove: false,
-                selection: false,
-                skipOffscreen: true,
-            });
-
-            this.onResize();
+            this.draw = SVG();
             window.addEventListener('resize', this.onResize);
         },
         beforeDestroy() {
             window.removeEventListener('resize', this.onResize);
         },
+
         methods: {
             formatDuration: formatDurationString,
-            draw: throttle(function () {
-                this.canvas.clear();
-
-                const width = this.canvas.getWidth();
-                const columnWidth = width / 7;
+            drawGrid: throttle(function () {
+                if (typeof this.draw === 'undefined') return;
+                this.draw.clear();
 
                 const startOfMonth = moment(this.start, 'YYYY-MM-DD').startOf('month');
                 const endOfMonth = moment(this.start, 'YYYY-MM-DD').endOf('month');
-
                 const firstDay = startOfMonth.clone().startOf('isoWeek');
                 const lastDay = endOfMonth.clone().endOf('isoWeek');
-
+                const canvasContainer = this.$refs.canvas;
+                const width = canvasContainer.clientWidth;
+                const columnWidth = width / 7;
                 const rows = lastDay.diff(firstDay, 'weeks') + 1;
+                const draw = this.draw;
+                draw.addTo(canvasContainer).size(width, headerHeight + rowHeight * 6);
 
-                // Background
-                this.canvas.add(
-                    new fabric.Rect({
-                        left: 0,
-                        top: headerHeight,
-                        width: width - 1,
-                        height: rows * rowHeight - 1,
-                        rx: 20,
-                        ry: 20,
-                        fill: '#FAFAFA',
-                        stroke: '#DFE5ED',
-                        strokeWidth: 1,
-                        ...fabricObjectOptions,
-                    }),
-                );
-
-                // Column headers
+                draw.rect(width - 1, (lastDay.diff(firstDay, 'weeks') + 1) * rowHeight - 1)
+                    .move(0, headerHeight)
+                    .radius(20)
+                    .fill('#FAFAFA');
                 for (let column = 0; column < columns; column++) {
                     const date = firstDay.clone().locale(this.$i18n.locale).add(column, 'days');
-                    this.canvas.add(
-                        new fabric.Textbox(date.format('dddd').toUpperCase(), {
-                            left: column * columnWidth,
-                            top: 0,
-                            width: columnWidth,
-                            height: headerHeight,
-                            textAlign: 'center',
-                            fontFamily: 'Nunito, sans-serif',
-                            fontSize: 10,
-                            fontWeight: 600,
+                    draw.text(date.format('dddd').toUpperCase())
+                        .move(column * columnWidth + columnWidth / 2, -5)
+                        .width(columnWidth)
+                        .height(headerHeight)
+                        .attr({
+                            'text-anchor': 'middle',
+                            'font-family': 'Nunito, sans-serif',
+                            'font-size': 10,
+                            'font-weight': 600,
                             fill: '#2E2EF9',
-                            ...fabricObjectOptions,
-                        }),
-                    );
+                        });
                 }
 
-                const { timePerDay } = this;
+                this.drawCells(draw, firstDay, columnWidth, rows, true, width, lastDay);
+                this.drawBackgroundGrid(draw, rows, width, columnWidth);
+                this.drawCells(draw, firstDay, columnWidth, rows, false, width, lastDay);
+            }),
+            drawBackgroundGrid(draw, rows, width, columnWidth) {
+                for (let row = 1; row < rows; row++) {
+                    draw.line(0, row * rowHeight + headerHeight, width, row * rowHeight + headerHeight).stroke({
+                        color: '#DFE5ED',
+                        width: 1,
+                    });
+                }
+
+                for (let column = 1; column < columns; column++) {
+                    draw.line(
+                        column * columnWidth,
+                        headerHeight,
+                        column * columnWidth,
+                        headerHeight + rowHeight * rows,
+                    ).stroke({
+                        color: '#DFE5ED',
+                        width: 1,
+                    });
+                }
+            },
+            drawCells(draw, firstDay, columnWidth, rows, cellBackground, width, lastDay) {
+                const squaresGroup = draw.group();
+
                 for (let row = 0; row < rows; row++) {
                     for (let column = 0; column < columns; column++) {
+                        const date = firstDay
+                            .clone()
+                            .locale(this.$i18n.locale)
+                            .add(row * columns + column, 'days');
                         const cellLeft = column * columnWidth;
                         const cellTop = headerHeight + row * rowHeight;
-                        const date = firstDay.clone().add(row * columns + column, 'days');
-
                         const isInSelection = date.diff(this.start) >= 0 && date.diff(this.end) <= 0;
-                        const isInSelectedMonth = date.diff(startOfMonth) >= 0 && date.diff(endOfMonth) <= 0;
+                        const { timePerDay } = this;
+                        if (isInSelection && cellBackground) {
+                            const square = draw.rect(columnWidth - 2, rowHeight - 2).attr({
+                                fill: '#F4F4FF',
+                                x: cellLeft + 1,
+                                y: cellTop + 1,
+                            });
 
-                        // Selected cell background
-                        if (isInSelection) {
-                            this.canvas.add(
-                                new fabric.Rect({
-                                    left: cellLeft + 1,
-                                    top: cellTop + 1,
-                                    width: columnWidth - 2,
-                                    height: rowHeight - 2,
-                                    fill: '#F4F4FF',
-                                    strokeWidth: 0,
-                                    clipPath: new fabric.Rect({
-                                        left: 0,
-                                        top: headerHeight,
-                                        width: width - 1,
-                                        height: rows * rowHeight - 1,
-                                        rx: 20,
-                                        ry: 20,
-                                        absolutePositioned: true,
-                                    }),
-                                    ...fabricObjectOptions,
-                                }),
-                            );
-                        }
+                            squaresGroup.add(square);
+                        } else {
+                            if (isInSelection) {
+                                const line = draw
+                                    .line(
+                                        column * columnWidth,
+                                        (row + 1) * rowHeight + headerHeight,
+                                        (column + 1) * columnWidth,
+                                        (row + 1) * rowHeight + headerHeight,
+                                    )
+                                    .stroke({
+                                        color: '#2E2EF9',
+                                        width: 3,
+                                    });
+                                squaresGroup.add(line);
+                            }
+                            draw.text(date.format('D'))
+                                .move((column + 1) * columnWidth - 10, row * rowHeight + headerHeight + 5)
+                                .attr({
+                                    'text-anchor': 'end',
+                                    'font-family': 'Nunito, sans-serif',
+                                    'font-size': 12,
+                                    'font-weight': isInSelection ? 600 : 400,
+                                    fill: isInSelection ? '#2E2EF9' : '#333333',
+                                });
 
-                        // Date label
-                        this.canvas.add(
-                            new fabric.Textbox(date.format('D'), {
-                                left: cellLeft,
-                                top: cellTop + 10,
-                                width: columnWidth - 13,
-                                height: rowHeight,
-                                textAlign: 'right',
-                                fontFamily: 'Nunito, sans-serif',
-                                fontSize: 15,
-                                fontWeight: isInSelection ? 600 : 400,
-                                fill: isInSelection ? '#2E2EF9' : isInSelectedMonth ? '#59566E' : '#B1B1BE',
-                                ...fabricObjectOptions,
-                            }),
-                        );
-
-                        // Worked time label
-                        const dateKey = date.format('YYYY-MM-DD');
-                        if (timePerDay[dateKey]) {
-                            this.canvas.add(
-                                new fabric.Textbox(this.formatDuration(timePerDay[dateKey]), {
-                                    left: cellLeft + 13,
-                                    top: cellTop + rowHeight - 30,
-                                    width: columnWidth,
-                                    height: rowHeight,
-                                    textAlign: 'left',
-                                    fontFamily: 'Nunito, sans-serif',
-                                    fontSize: 15,
-                                    fontWeight: isInSelection ? 600 : 400,
-                                    fill: '#59566E',
-                                    ...fabricObjectOptions,
-                                }),
-                            );
-                        }
-
-                        // Selected cell bottom border
-                        if (isInSelection) {
-                            this.canvas.add(
-                                new fabric.Line([0, 0, columnWidth, 0], {
-                                    left: columnWidth * column,
-                                    top: cellTop + rowHeight - 3,
-                                    stroke: '#2E2EF9',
-                                    strokeWidth: 3,
-                                    rx: 1.5,
-                                    ry: 1.5,
-                                    clipPath: new fabric.Rect({
-                                        left: 0,
-                                        top: headerHeight,
-                                        width: width - 1,
-                                        height: rows * rowHeight - 1,
-                                        rx: 20,
-                                        ry: 20,
-                                        absolutePositioned: true,
-                                    }),
-                                    ...fabricObjectOptions,
-                                }),
-                            );
+                            const dateKey = date.format('YYYY-MM-DD');
+                            if (timePerDay[dateKey] && isInSelection) {
+                                draw.text(this.formatDuration(timePerDay[dateKey]))
+                                    .move(cellLeft + 13, cellTop + rowHeight - 30)
+                                    .attr({
+                                        'text-anchor': 'inherit',
+                                        'font-family': 'Nunito, sans-serif',
+                                        'font-size': 15,
+                                        'font-weight': 600,
+                                        fill: '#59566E',
+                                    });
+                            }
                         }
                     }
                 }
-
-                // Horizontal grid lines
-                for (let row = 1; row < rows; row++) {
-                    this.canvas.add(
-                        new fabric.Line([0, 0, width, 0], {
-                            left: 0,
-                            top: rowHeight * row + headerHeight,
-                            stroke: '#DFE5ED',
-                            strokeWidth: 1,
-                            ...fabricObjectOptions,
-                        }),
-                    );
-                }
-
-                // Vertical grid lines
-                for (let column = 1; column < columns; column++) {
-                    this.canvas.add(
-                        new fabric.Line([0, 0, 0, rowHeight * rows], {
-                            left: columnWidth * column,
-                            top: 20,
-                            stroke: '#DFE5ED',
-                            strokeWidth: 1,
-                            ...fabricObjectOptions,
-                        }),
-                    );
-                }
-
-                this.canvas.requestRenderAll();
-            }),
+                let clip = draw.clip();
+                clip.add(
+                    draw
+                        .rect(width - 1, (lastDay.diff(firstDay, 'weeks') + 1) * rowHeight + 1)
+                        .move(0, headerHeight)
+                        .radius(20),
+                );
+                squaresGroup.clipWith(clip);
+            },
             onResize: throttle(function () {
-                if (!this.$refs.canvasWrapper) {
-                    return;
-                }
-                const { width } = this.$refs.canvasWrapper.getBoundingClientRect();
-                this.canvas.setWidth(width);
-
-                const startOfMonth = moment(this.start, 'YYYY-MM-DD').startOf('month');
-                const endOfMonth = moment(this.start, 'YYYY-MM-DD').endOf('month');
-                const firstDay = startOfMonth.startOf('isoWeek');
-                const lastDay = endOfMonth.endOf('isoWeek');
-                const rows = lastDay.diff(firstDay, 'weeks') + 1;
-                const height = headerHeight + rowHeight * rows;
-                this.canvas.setHeight(height);
-
-                this.draw();
-            }, 100),
+                this.drawGrid();
+            }, 0),
         },
+
         watch: {
             start() {
                 this.onResize();
@@ -255,14 +178,14 @@
                 this.onResize();
             },
             timePerDay() {
-                this.draw();
+                this.drawGrid();
             },
         },
     };
 </script>
 
 <style lang="scss" scoped>
-    .canvas::v-deep canvas {
-        box-sizing: content-box;
+    .canvas ::v-deep svg {
+        width: 100%;
     }
 </style>
