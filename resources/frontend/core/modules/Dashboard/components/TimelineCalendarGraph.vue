5f659d24d8ae2eb13ea4b864ca39fa581661cf27
Danil Shapilov <danil_shapilov@amazingcat.net>
2024-04-16 03:32:32 +0500

    fix: timeline calendar

--- resources/frontend/core/modules/Dashboard/components/TimelineCalendarGraph.vue
+++ resources/frontend/core/modules/Dashboard/components/TimelineCalendarGraph.vue
@@ -3,10 +3,11 @@
 </template>
 
 <script>
-    import throttle from 'lodash/throttle';
     import moment from 'moment';
     import { formatDurationString } from '@/utils/time';
     import { SVG } from '@svgdotjs/svg.js';
+    import debounce from 'lodash/debounce';
+
     const headerHeight = 20;
     const columns = 7;
     const rowHeight = 120;
@@ -30,6 +31,7 @@
         mounted() {
             this.draw = SVG();
             window.addEventListener('resize', this.onResize);
+            this.drawGrid();
         },
         beforeDestroy() {
             window.removeEventListener('resize', this.onResize);
@@ -37,48 +39,51 @@
 
         methods: {
             formatDuration: formatDurationString,
-            drawGrid: throttle(function () {
-                if (typeof this.draw === 'undefined') return;
-                this.draw.clear();
+            drawGrid: debounce(
+                function () {
+                    if (typeof this.draw === 'undefined') return;
+                    this.draw.clear();
 
-                const startOfMonth = moment(this.start, 'YYYY-MM-DD').startOf('month');
-                const endOfMonth = moment(this.start, 'YYYY-MM-DD').endOf('month');
-                const firstDay = startOfMonth.clone().startOf('isoWeek');
-                const lastDay = endOfMonth.clone().endOf('isoWeek');
-                const canvasContainer = this.$refs.canvas;
-                const width = canvasContainer.clientWidth;
-                const columnWidth = width / 7;
-                const rows = lastDay.diff(firstDay, 'weeks') + 1;
-                const draw = this.draw;
-                draw.addTo(canvasContainer).size(width, headerHeight + rowHeight * 6);
+                    const startOfMonth = moment(this.start, 'YYYY-MM-DD').startOf('month');
+                    const endOfMonth = moment(this.start, 'YYYY-MM-DD').endOf('month');
+                    const firstDay = startOfMonth.clone().startOf('isoWeek');
+                    const lastDay = endOfMonth.clone().endOf('isoWeek');
+                    const canvasContainer = this.$refs.canvas;
+                    const width = canvasContainer.clientWidth;
+                    const columnWidth = width / 7;
+                    const rows = lastDay.diff(firstDay, 'weeks') + 1;
+                    const draw = this.draw;
+                    draw.addTo(canvasContainer).size(width, headerHeight + rowHeight * 6);
 
-                draw.rect(width - 1, (lastDay.diff(firstDay, 'weeks') + 1) * rowHeight - 1)
-                    .move(0, headerHeight)
-                    .radius(20)
-                    .fill('#FAFAFA')
-                    .stroke({ color: '#dfe5ed', width: 1 });
-                for (let column = 0; column < columns; column++) {
-                    const date = firstDay.clone().locale(this.$i18n.locale).add(column, 'days');
-                    draw.text(date.format('dddd').toUpperCase())
-                        .move(column * columnWidth + columnWidth / 2, -5)
-                        .width(columnWidth)
-                        .height(headerHeight)
-                        .attr({
-                            'text-anchor': 'middle',
-                            'font-family': 'Nunito, sans-serif',
-                            'font-size': 10,
-                            'font-weight': 600,
-                            fill: '#2E2EF9',
-                        });
-                }
+                    draw.rect(width - 2, (lastDay.diff(firstDay, 'weeks') + 1) * rowHeight - 1)
+                        .move(1, headerHeight)
+                        .radius(20)
+                        .fill('#FAFAFA')
+                        .stroke({ color: '#dfe5ed', width: 1 });
+                    for (let column = 0; column < columns; column++) {
+                        const date = firstDay.clone().locale(this.$i18n.locale).add(column, 'days');
+                        draw.text(date.format('dddd').toUpperCase())
+                            .move(column * columnWidth + columnWidth / 2, -5)
+                            .width(columnWidth)
+                            .height(headerHeight)
+                            .attr({
+                                'text-anchor': 'middle',
+                                'font-family': 'Nunito, sans-serif',
+                                'font-size': 10,
+                                'font-weight': 600,
+                                fill: '#2E2EF9',
+                            });
+                    }
 
-                this.drawCells(draw, firstDay, columnWidth, rows, true, width, lastDay);
-                this.drawBackgroundGrid(draw, rows, width, columnWidth);
-                this.drawCells(draw, firstDay, columnWidth, rows, false, width, lastDay);
-            }),
+                    this.drawCells(draw, firstDay, columnWidth, rows, width, lastDay);
+                    this.drawBackgroundGrid(draw, rows, width, columnWidth);
+                },
+                30,
+                { maxWait: 50 },
+            ),
             drawBackgroundGrid(draw, rows, width, columnWidth) {
                 for (let row = 1; row < rows; row++) {
-                    draw.line(0, row * rowHeight + headerHeight, width, row * rowHeight + headerHeight).stroke({
+                    draw.line(1, row * rowHeight + headerHeight, width - 1, row * rowHeight + headerHeight).stroke({
                         color: '#DFE5ED',
                         width: 1,
                     });
@@ -89,14 +94,14 @@
                         column * columnWidth,
                         headerHeight,
                         column * columnWidth,
-                        headerHeight + rowHeight * rows,
+                        headerHeight + rowHeight * rows - 1,
                     ).stroke({
                         color: '#DFE5ED',
                         width: 1,
                     });
                 }
             },
-            drawCells(draw, firstDay, columnWidth, rows, cellBackground, width, lastDay) {
+            drawCells(draw, firstDay, columnWidth, rows, width, lastDay) {
                 const squaresGroup = draw.group();
 
                 for (let row = 0; row < rows; row++) {
@@ -109,7 +114,8 @@
                         const cellTop = headerHeight + row * rowHeight;
                         const isInSelection = date.diff(this.start) >= 0 && date.diff(this.end) <= 0;
                         const { timePerDay } = this;
-                        if (isInSelection && cellBackground) {
+
+                        if (isInSelection) {
                             const square = draw.rect(columnWidth - 2, rowHeight - 2).attr({
                                 fill: '#F4F4FF',
                                 x: cellLeft + 1,
@@ -117,58 +123,56 @@
                             });
 
                             squaresGroup.add(square);
-                        } else {
-                            if (isInSelection) {
-                                const line = draw
-                                    .line(
-                                        column * columnWidth,
-                                        (row + 1) * rowHeight + headerHeight,
-                                        (column + 1) * columnWidth,
-                                        (row + 1) * rowHeight + headerHeight,
-                                    )
-                                    .stroke({
-                                        color: '#2E2EF9',
-                                        width: 3,
-                                    });
-                                squaresGroup.add(line);
-                            }
-                            draw.text(date.format('D'))
-                                .move((column + 1) * columnWidth - 10, row * rowHeight + headerHeight + 5)
-                                .attr({
-                                    'text-anchor': 'end',
-                                    'font-family': 'Nunito, sans-serif',
-                                    'font-size': 12,
-                                    'font-weight': isInSelection ? 600 : 400,
-                                    fill: isInSelection ? '#2E2EF9' : '#868495',
-                                });
 
-                            const dateKey = date.format('YYYY-MM-DD');
-                            if (timePerDay[dateKey] && isInSelection) {
-                                draw.text(this.formatDuration(timePerDay[dateKey]))
-                                    .move(cellLeft + 13, cellTop + rowHeight - 30)
-                                    .attr({
-                                        'text-anchor': 'inherit',
-                                        'font-family': 'Nunito, sans-serif',
-                                        'font-size': 15,
-                                        'font-weight': 600,
-                                        fill: '#59566E',
-                                    });
-                            }
+                            const line = draw
+                                .line(
+                                    column * columnWidth,
+                                    (row + 1) * rowHeight + headerHeight - 2,
+                                    (column + 1) * columnWidth,
+                                    (row + 1) * rowHeight + headerHeight - 2,
+                                )
+                                .stroke({
+                                    color: '#2E2EF9',
+                                    width: 3,
+                                });
+                            squaresGroup.add(line);
+                        }
+                        draw.text(date.format('D'))
+                            .move((column + 1) * columnWidth - 10, row * rowHeight + headerHeight + 5)
+                            .attr({
+                                'text-anchor': 'end',
+                                'font-family': 'Nunito, sans-serif',
+                                'font-size': 12,
+                                'font-weight': isInSelection ? 600 : 400,
+                                fill: isInSelection ? '#2E2EF9' : '#868495',
+                            });
+
+                        const dateKey = date.format('YYYY-MM-DD');
+                        if (timePerDay[dateKey]) {
+                            draw.text(this.formatDuration(timePerDay[dateKey]))
+                                .move(cellLeft + 13, cellTop + rowHeight - 30)
+                                .attr({
+                                    'text-anchor': 'inherit',
+                                    'font-family': 'Nunito, sans-serif',
+                                    'font-size': 15,
+                                    'font-weight': isInSelection ? 600 : 400,
+                                    fill: '#59566E',
+                                });
                         }
                     }
                 }
                 let clip = draw.clip();
                 clip.add(
                     draw
-                        .rect(width - 1, (lastDay.diff(firstDay, 'weeks') + 1) * rowHeight + 1)
-                        .move(0, headerHeight)
+                        .rect(width - 4, (lastDay.diff(firstDay, 'weeks') + 1) * rowHeight - 1.5)
+                        .move(2, headerHeight)
                         .radius(20),
                 );
                 squaresGroup.clipWith(clip);
             },
-            onResize: throttle(function () {
+            onResize: function () {
                 this.drawGrid();
-            }, 0),
+            },
         },
 
         watch: {
