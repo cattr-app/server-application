df2d649d7a6d600b1029bd956851d73465056882
Alexey Kuznetsov <alexey@amazingcat.net>
2024-10-13 18:28:54 +0600

    fix: split calendar component

--- resources/frontend/core/modules/Calendar/views/Calendar.vue
+++ resources/frontend/core/modules/Calendar/views/Calendar.vue
@@ -1,28 +1,25 @@
 <template>
     <div>
-        <div ref="container" class="svg-container svg-container__desktop"></div>
-        <div ref="mobile-container" class="svg-container svg-container__mobile"></div>
+        <calendar-mobile-view class="svg-container svg-container__mobile" :tasks-by-day="tasksByDay" />
+        <calendar-view class="svg-container svg-container__desktop" :tasks-by-week="tasksByWeek" />
     </div>
 </template>
 
 <script>
-    import { Svg, SVG } from '@svgdotjs/svg.js';
     import moment from 'moment';
+    import CalendarMobileView from './CalendarMobileView.vue';
+    import CalendarView from './CalendarView.vue';
     import CalendarService from '../services/calendar.service';
 
-    const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-
-    const cellWidth = 100 / daysOfWeek.length;
-    const cellHeight = 32;
-
-    const maxWidth = 1200;
-
     export default {
+        components: {
+            CalendarMobileView,
+            CalendarView,
+        },
         data() {
             return {
-                tasks: {},
-                tasksByDay: {},
-                tasksByWeek: {},
+                tasksByDay: [],
+                tasksByWeek: [],
             };
         },
         created() {
@@ -30,241 +27,19 @@
             this.service
                 .get(moment().startOf('month').format('YYYY-MM-DD'), moment().endOf('month').format('YYYY-MM-DD'))
                 .then(response => {
-                    this.tasks = response.data.data.tasks;
-                    this.tasksByDay = response.data.data.tasks_by_day;
-                    this.tasksByWeek = response.data.data.tasks_by_week;
+                    this.tasksByDay = Object.values(response.data.data.tasks_by_day).map(dayItem => ({
+                        ...dayItem,
+                        tasks: dayItem.task_ids.map(task_id => response.data.data.tasks[task_id]),
+                    }));
 
-                    this.resize();
-                    this.draw();
+                    this.tasksByWeek = Object.values(response.data.data.tasks_by_week).map(weekData => ({
+                        ...weekData,
+                        tasks: weekData.tasks.map(taskData => ({
+                            ...taskData,
+                            task: response.data.data.tasks[taskData.task_id],
+                        })),
+                    }));
                 });
-
-            window.addEventListener('resize', this.resize);
-        },
-        mounted() {
-            const { container, 'mobile-container': mobileContainer } = this.$refs;
-            this.svg = SVG().addTo(container);
-            this.mobileSvg = SVG().addTo(mobileContainer);
-
-            this.resize();
-            this.draw();
-        },
-        destroyed() {
-            window.removeEventListener('resize', this.resize);
-        },
-        methods: {
-            resize() {
-                this.resizeMobile();
-                this.resizeDesktop();
-            },
-            resizeMobile() {
-                const { 'mobile-container': mobileContainer } = this.$refs;
-
-                const weeks = Object.keys(this.tasksByWeek).length;
-                const rows = 1 + 2 * weeks;
-
-                const width = Math.min(maxWidth, mobileContainer.clientWidth);
-                const height = rows * cellHeight;
-
-                this.mobileSvg.viewbox(0, 0, width, height);
-            },
-            resizeDesktop() {
-                const { container } = this.$refs;
-
-                const weeks = Object.keys(this.tasksByWeek).length;
-                const tasks = Object.values(this.tasksByWeek).reduce((acc, item) => acc + item.tasks.length, 0);
-                const rows = 1 + weeks + tasks;
-
-                const width = Math.min(maxWidth, container.clientWidth);
-                const height = rows * cellHeight;
-
-                this.svg.viewbox(0, 0, width, height);
-            },
-            draw() {
-                this.drawMobile();
-                this.drawDesktop();
-            },
-            drawMobile() {
-                /** @type {Svg} */
-                const svg = this.mobileSvg;
-                svg.clear();
-
-                const group = svg.group();
-                group.clipWith(svg.rect('100%', '100%').rx(20).ry(20));
-
-                let horizontalOffset = 0;
-                let verticalOffset = 0;
-
-                const showTasksModal = tasks => {
-                    // TODO: show modal
-                    console.log(tasks);
-                };
-
-                const drawHeader = () => {
-                    for (const day of daysOfWeek) {
-                        const rect = group
-                            .rect(`${cellWidth.toFixed(2)}%`, cellHeight)
-                            .move(`${horizontalOffset.toFixed(2)}%`, verticalOffset)
-                            .fill('#fff')
-                            .stroke('#F4F4FF');
-
-                        group
-                            .text(add => add.tspan(this.$t(`calendar.days.${day}`)))
-                            .font({ anchor: 'middle', size: 16 })
-                            .amove(`${(horizontalOffset + cellWidth / 2).toFixed(2)}%`, verticalOffset + cellHeight / 2)
-                            .fill('rgb(177, 177, 190)')
-                            .clipWith(rect.clone());
-
-                        horizontalOffset += cellWidth;
-                    }
-
-                    verticalOffset += cellHeight;
-                };
-
-                const drawDays = () => {
-                    horizontalOffset = 0;
-
-                    const days = Object.values(this.tasksByDay);
-                    for (let i = 0; i < days.length; i++) {
-                        const { day, task_ids: taskIds } = days[i];
-
-                        const onClick = () => showTasksModal(taskIds.map(taskId => this.tasks[taskId]));
-
-                        const rect = group
-                            .rect(`${cellWidth.toFixed(2)}%`, 2 * cellHeight)
-                            .move(`${horizontalOffset.toFixed(2)}%`, verticalOffset)
-                            .fill('#fff')
-                            .stroke('#F4F4FF')
-                            .on('click', onClick);
-
-                        group
-                            .text(add => add.tspan(day.toString()))
-                            .font({ anchor: 'middle', size: 16 })
-                            .amove(`${(horizontalOffset + cellWidth / 2).toFixed(2)}%`, verticalOffset + cellHeight / 2)
-                            .fill('rgb(63, 83, 110)')
-                            .clipWith(rect.clone())
-                            .on('click', onClick);
-
-                        if (taskIds.length > 0) {
-                            group
-                                .circle(10)
-                                .attr('cx', `${(horizontalOffset + cellWidth / 2).toFixed(2)}%`)
-                                .attr('cy', verticalOffset + cellHeight * 1.5)
-                                .fill('rgb(177, 177, 190)')
-                                .on('click', onClick);
-                        }
-
-                        if (i % daysOfWeek.length === daysOfWeek.length - 1) {
-                            horizontalOffset = 0;
-                            verticalOffset += 2 * cellHeight;
-                        } else {
-                            horizontalOffset += cellWidth;
-                        }
-                    }
-                };
-
-                drawHeader();
-                drawDays();
-            },
-            drawDesktop() {
-                /** @type {Svg} */
-                const svg = this.svg;
-                svg.clear();
-
-                const group = svg.group();
-                group.clipWith(svg.rect('100%', '100%').rx(20).ry(20));
-
-                let horizontalOffset = 0;
-                let verticalOffset = 0;
-
-                const drawHeader = () => {
-                    for (const day of daysOfWeek) {
-                        const rect = group
-                            .rect(`${cellWidth.toFixed(2)}%`, cellHeight)
-                            .move(`${horizontalOffset.toFixed(2)}%`, verticalOffset)
-                            .fill('#fff')
-                            .stroke('#F4F4FF');
-
-                        group
-                            .text(add => add.tspan(this.$t(`calendar.days.${day}`)))
-                            .font({ anchor: 'middle', size: 16 })
-                            .amove(`${(horizontalOffset + cellWidth / 2).toFixed(2)}%`, verticalOffset + cellHeight / 2)
-                            .fill('rgb(177, 177, 190)')
-                            .clipWith(rect.clone());
-
-                        horizontalOffset += cellWidth;
-                    }
-
-                    verticalOffset += cellHeight;
-                };
-
-                /**
-                 * @param {string[]} days
-                 */
-                const drawDaysRow = days => {
-                    horizontalOffset = 0;
-
-                    for (const day of days) {
-                        const rect = group
-                            .rect(`${cellWidth.toFixed(2)}%`, cellHeight)
-                            .move(`${horizontalOffset.toFixed(2)}%`, verticalOffset)
-                            .fill('#fff')
-                            .stroke('#F4F4FF');
-
-                        group
-                            .text(add => add.tspan(day.toString()).dmove(-8, 0))
-                            .font({ anchor: 'end', size: 16 })
-                            .amove(`${(horizontalOffset + cellWidth).toFixed(2)}%`, verticalOffset + cellHeight / 2)
-                            .fill('rgb(63, 83, 110)')
-                            .clipWith(rect.clone());
-
-                        horizontalOffset += cellWidth;
-                    }
-
-                    verticalOffset += cellHeight;
-                };
-
-                /**
-                 * @param {number} taskId
-                 * @param {number} startWeekDay
-                 * @param {number} endWeekDay
-                 */
-                const drawTaskRow = (taskId, startWeekDay, endWeekDay) => {
-                    const width = cellWidth * (endWeekDay - startWeekDay + 1);
-                    horizontalOffset = cellWidth * startWeekDay;
-
-                    group.rect(`100%`, cellHeight).move(0, verticalOffset).fill('#fafafa').stroke('#F4F4FF');
-
-                    const link = group.link(`/tasks/view/${taskId}`);
-                    link.target('_blank');
-
-                    const taskHorizontalPadding = 0.25;
-                    const taskVerticaladding = 2;
-
-                    const rect = link
-                        .rect(`${width - 2 * taskHorizontalPadding}%`, cellHeight - 2 * taskVerticaladding)
-                        .move(`${horizontalOffset + taskHorizontalPadding}%`, verticalOffset + taskVerticaladding)
-                        .fill('#fff')
-                        .stroke('#F4F4FF');
-
-                    link.text(add => add.tspan(this.tasks[taskId].task_name).dmove(8, 0))
-                        .font({ anchor: 'start', size: 16 })
-                        .amove(`${horizontalOffset + taskHorizontalPadding}%`, verticalOffset + cellHeight / 2)
-                        .fill('rgb(63, 83, 110)')
-                        .clipWith(rect.clone());
-
-                    verticalOffset += cellHeight;
-                };
-
-                drawHeader();
-
-                for (const { days, tasks } of Object.values(this.tasksByWeek)) {
-                    drawDaysRow(days);
-
-                    for (const { task_id, start_week_day, end_week_day } of tasks) {
-                        drawTaskRow(task_id, start_week_day, end_week_day);
-                    }
-                }
-            },
         },
     };
 </script>
---
+++ resources/frontend/core/modules/Calendar/views/CalendarView.vue
@@ -0,0 +1,163 @@
+<template>
+    <div ref="container" class="svg-container"></div>
+</template>
+
+<script>
+    import { Svg, SVG } from '@svgdotjs/svg.js';
+    import throttle from 'lodash/throttle';
+
+    const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+    const cellWidth = 100 / daysOfWeek.length;
+    const cellHeight = 32;
+
+    const maxWidth = 1200;
+
+    export default {
+        props: {
+            tasksByWeek: {
+                type: Array,
+                required: true,
+                default: () => [],
+            },
+        },
+        created() {
+            window.addEventListener('resize', this.resize);
+        },
+        mounted() {
+            const { container } = this.$refs;
+            this.svg = SVG().addTo(container);
+
+            this.resize();
+            this.draw();
+        },
+        destroyed() {
+            window.removeEventListener('resize', this.resize);
+        },
+        watch: {
+            tasksByWeek() {
+                this.resize();
+                this.draw();
+            },
+        },
+        methods: {
+            resize: throttle(function () {
+                const { container } = this.$refs;
+
+                const weeks = this.tasksByWeek.length;
+                const tasks = this.tasksByWeek.reduce((acc, item) => acc + item.tasks.length, 0);
+                const rows = 1 + weeks + tasks;
+
+                const width = Math.min(maxWidth, container.clientWidth);
+                const height = rows * cellHeight;
+
+                this.svg.viewbox(0, 0, width, height);
+            }, 100),
+            draw: throttle(function () {
+                /** @type {Svg} */
+                const svg = this.svg;
+                svg.clear();
+
+                const group = svg.group();
+                group.clipWith(svg.rect('100%', '100%').rx(20).ry(20));
+
+                let horizontalOffset = 0;
+                let verticalOffset = 0;
+
+                const drawHeader = () => {
+                    for (const day of daysOfWeek) {
+                        const rect = group
+                            .rect(`${cellWidth.toFixed(2)}%`, cellHeight)
+                            .move(`${horizontalOffset.toFixed(2)}%`, verticalOffset)
+                            .fill('#fff')
+                            .stroke('#F4F4FF');
+
+                        group
+                            .text(add => add.tspan(this.$t(`calendar.days.${day}`)))
+                            .font({ anchor: 'middle', size: 16 })
+                            .amove(`${(horizontalOffset + cellWidth / 2).toFixed(2)}%`, verticalOffset + cellHeight / 2)
+                            .fill('rgb(177, 177, 190)')
+                            .clipWith(rect.clone());
+
+                        horizontalOffset += cellWidth;
+                    }
+
+                    verticalOffset += cellHeight;
+                };
+
+                /**
+                 * @param {string[]} days
+                 */
+                const drawDaysRow = days => {
+                    horizontalOffset = 0;
+
+                    for (const day of days) {
+                        const rect = group
+                            .rect(`${cellWidth.toFixed(2)}%`, cellHeight)
+                            .move(`${horizontalOffset.toFixed(2)}%`, verticalOffset)
+                            .fill('#fff')
+                            .stroke('#F4F4FF');
+
+                        group
+                            .text(add => add.tspan(day.toString()).dmove(-8, 0))
+                            .font({ anchor: 'end', size: 16 })
+                            .amove(`${(horizontalOffset + cellWidth).toFixed(2)}%`, verticalOffset + cellHeight / 2)
+                            .fill('rgb(63, 83, 110)')
+                            .clipWith(rect.clone());
+
+                        horizontalOffset += cellWidth;
+                    }
+
+                    verticalOffset += cellHeight;
+                };
+
+                /**
+                 * @param {number} task_id
+                 * @param {string} task_name
+                 * @param {number} startWeekDay
+                 * @param {number} endWeekDay
+                 */
+                const drawTaskRow = (task_id, task_name, startWeekDay, endWeekDay) => {
+                    const width = cellWidth * (endWeekDay - startWeekDay + 1);
+                    horizontalOffset = cellWidth * startWeekDay;
+
+                    group.rect(`100%`, cellHeight).move(0, verticalOffset).fill('#fafafa').stroke('#F4F4FF');
+
+                    const link = group.link(`/tasks/view/${task_id}`);
+                    link.target('_blank');
+
+                    const taskHorizontalPadding = 0.25;
+                    const taskVerticaladding = 2;
+
+                    const rect = link
+                        .rect(`${width - 2 * taskHorizontalPadding}%`, cellHeight - 2 * taskVerticaladding)
+                        .move(`${horizontalOffset + taskHorizontalPadding}%`, verticalOffset + taskVerticaladding)
+                        .fill('#fff')
+                        .stroke('#F4F4FF');
+
+                    link.text(add => add.tspan(task_name).dmove(8, 0))
+                        .font({ anchor: 'start', size: 16 })
+                        .amove(`${horizontalOffset + taskHorizontalPadding}%`, verticalOffset + cellHeight / 2)
+                        .fill('rgb(63, 83, 110)')
+                        .clipWith(rect.clone());
+
+                    verticalOffset += cellHeight;
+                };
+
+                drawHeader();
+
+                for (const { days, tasks } of this.tasksByWeek) {
+                    drawDaysRow(days);
+
+                    for (const {
+                        task: { id, task_name },
+                        start_week_day,
+                        end_week_day,
+                    } of tasks) {
+                        drawTaskRow(id, task_name, start_week_day, end_week_day);
+                    }
+                }
+            }, 100),
+        },
+    };
+</script>
