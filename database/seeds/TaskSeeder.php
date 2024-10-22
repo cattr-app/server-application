a4ad5768e3997525aa64e579262a96d398d9090c
OlegDenisov95 <hanonhanon95@gmail.com>
2024-10-22 13:23:45 +0600

    fix: seed

---
+++ database/seeds/TaskSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    public function run()
+    {
+        Task::factory()
+            ->count(10)
+            ->create();
+    }
+}
