a83022bb64d4d57a173956b6f633c77720cdf638
Danil Shapilov <danil_shapilov@amazingcat.net>
2024-10-22 08:24:09 +0000

    Revert "fix: seed"
    This reverts commit a4ad5768e3997525aa64e579262a96d398d9090c

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -4,12 +4,10 @@ namespace Database\Factories;
 
 use App\Enums\Role;
 use App\Models\Priority;
-use App\Models\Project;
 use App\Models\Status;
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Carbon;
 
 class TaskFactory extends Factory
 {
@@ -27,17 +25,13 @@ class TaskFactory extends Factory
      */
     public function definition(): array
     {
-        $project = Project::query()->inRandomOrder()->first();
         return [
-            'project_id' =>  $project->id,
             'task_name' => fake()->sentence(3),
             'description' => fake()->paragraph,
             'assigned_by' => fn() => User::where(['role_id' => Role::ADMIN])->first()->id,
             'important' => fake()->boolean,
             'priority_id' => Priority::inRandomOrder()->first()->id,
             'status_id' => Status::inRandomOrder()->first()->id,
-            'start_date' => Carbon::now(),
-            'due_date' => Carbon::now()->addDays($this->faker->numberBetween(1,30)),
         ];
     }
 }
